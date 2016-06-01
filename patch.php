9d00713777b0af62581d28a0468aa83615b730b5
Eugene Tulika <vranen@gmail.com>
2016-06-01 15:58:40 -0500

    update to 2.1 version

--- patch.php
+++ patch.php
@@ -14,3 +14,4 @@ foreach ($dir as $fileinfo) {
 $root = __DIR__ . '/../../../';
 copy($root . 'app/etc/NonComposerComponentRegistration.php', $root . 'app/NonComposerComponentRegistration.php');
 copy($root . 'app/etc/di.xml', $root . 'app/di.xml');
+copy($root . 'app/etc/enterprise', $root . 'app/enterprise');
