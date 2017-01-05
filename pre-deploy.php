e9e51be3725cb7d4ac269ada6dd509ae1c609318
Maddy Chellathurai <mchellathura@magento.com>
2017-01-05 16:53:52 +0000

    MAGECLOUD-278 Put SCD in build step - cleaning up patches

--- pre-deploy.php
+++ pre-deploy.php
@@ -71,15 +71,4 @@ if ($useStaticContentSymlink) {
     }
 }
 
-
-///**
-// * Remove application configuration files whose information should be represented in the DB
-// */
-//$etcDir = realpath(Environment::MAGENTO_ROOT . 'app/etc/') . '/';
-//$appConfigFiles = ['setup.config.php', 'scope.config.php'];
-//foreach ($appConfigFiles as $file) {
-//    if (file_exists($etcDir . $file)) {
-//        unlink($etcDir . $file);
-//    }
-//}
 $env->log("Pre-deploy complete.");
