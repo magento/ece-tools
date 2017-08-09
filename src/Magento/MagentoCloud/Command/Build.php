935db8d42c6bd4186f1de8a0973c623af3b5e73d
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2017-08-09 16:01:55 +0300

    MAGECLOUD-952: Create StaticContentDeploy process for build command

--- src/Magento/MagentoCloud/Command/Build.php
+++ src/Magento/MagentoCloud/Command/Build.php
@@ -8,9 +8,11 @@ namespace Magento\MagentoCloud\Command;
 
 use Magento\MagentoCloud\Environment;
 use Magento\MagentoCloud\Process\ProcessInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Magento\MagentoCloud\Config\Build as BuildConfig;
 
 /**
  * CLI command for build hook. Responsible for preparing the codebase before it's moved to the server.
@@ -18,44 +20,35 @@ use Symfony\Component\Console\Output\OutputInterface;
 class Build extends Command
 {
     /**
-     * Options for build_options.ini
-     */
-    const BUILD_OPT_SKIP_DI_COMPILATION = 'skip_di_compilation';
-    const BUILD_OPT_SKIP_DI_CLEARING = 'skip_di_clearing';
-    const BUILD_OPT_SCD_EXCLUDE_THEMES = 'exclude_themes';
-    const BUILD_OPT_SCD_THREADS = 'scd_threads';
-    const BUILD_OPT_SKIP_SCD = 'skip_scd';
-
-    /**
-     * @var Environment
-     */
-    private $env;
-
-    /**
-     * @var array
-     */
-    private $buildOptions;
-
-    /**
-     * @var string
-     */
-    private $verbosityLevel;
-
-    /**
      * @var ProcessInterface
      */
     private $process;
 
-    public function __construct(ProcessInterface $process)
-    {
+    /**
+     * @var BuildConfig
+     */
+    private $buildConfig;
+
+    /**
+     * @var Environment
+     */
+    private $environment;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(
+        ProcessInterface $process,
+        BuildConfig $buildConfig,
+        Environment $environment,
+        LoggerInterface $logger
+    ) {
         $this->process = $process;
-        $this->buildOptions = $this->parseBuildOptions();
-        $this->env = new Environment();
-        $buildVerbosityLevel = $this->getBuildOption('VERBOSE_COMMANDS');
-        isset($buildVerbosityLevel) && $buildVerbosityLevel == 'enabled'
-            ? $this->env->log("Verbosity level is set to " . $buildVerbosityLevel)
-            : $this->env->log("Verbosity level is not set");
-        $this->verbosityLevel = isset($buildVerbosityLevel) && $buildVerbosityLevel == 'enabled' ? ' -vv ' : '';
+        $this->buildConfig = $buildConfig;
+        $this->environment = $environment;
+        $this->logger = $logger;
 
         parent::__construct();
     }
@@ -77,8 +70,11 @@ class Build extends Command
     public function execute(InputInterface $input, OutputInterface $output)
     {
         try {
-            $this->env->setStaticDeployInBuild(false);
-            $this->env->log($this->env->startingMessage("build"));
+            $buildVerbosityLevel = $this->buildConfig->getVerbosityLevel();
+            $this->logger->info('Verbosity level is ' . $buildVerbosityLevel ?: 'not set');
+
+            $this->environment->setStaticDeployInBuild(false);
+            $this->logger->info($this->environment->startingMessage("build"));
 
             $this->process->execute();
         } catch (\Exception $exception) {
@@ -86,22 +82,7 @@ class Build extends Command
 
             return $exception->getCode();
         }
-    }
 
-    /**
-     * Parse optional build_options.ini file in Magento root directory
-     */
-    private function parseBuildOptions()
-    {
-        $fileName = Environment::MAGENTO_ROOT . '/build_options.ini';
-
-        return file_exists($fileName)
-            ? parse_ini_file(Environment::MAGENTO_ROOT . '/build_options.ini')
-            : [];
-    }
-
-    private function getBuildOption($key)
-    {
-        return isset($this->buildOptions[$key]) ? $this->buildOptions[$key] : false;
+        return 0;
     }
 }
