d0e9ad1baae5aa922e5d38b1f0dc4878799fba2f
Oleh Posyniak <oposyniak@magento.com>
2017-09-01 12:59:14 +0300

    MAGECLOUD-946: Cover with Unit tests

--- src/Magento/MagentoCloud/Process/Deploy/DeployStaticContent.php
+++ src/Magento/MagentoCloud/Process/Deploy/DeployStaticContent.php
@@ -6,10 +6,7 @@
 namespace Magento\MagentoCloud\Process\Deploy;
 
 use Magento\MagentoCloud\Config\Environment;
-use Magento\MagentoCloud\DB\Adapter;
-use Magento\MagentoCloud\Filesystem\DirectoryList;
 use Magento\MagentoCloud\Process\ProcessInterface;
-use Magento\MagentoCloud\Shell\ShellInterface;
 use Magento\MagentoCloud\Util\StaticContentCleaner;
 use Psr\Log\LoggerInterface;
 
@@ -19,14 +16,14 @@ use Psr\Log\LoggerInterface;
 class DeployStaticContent implements ProcessInterface
 {
     /**
+     * @var ProcessInterface
+     */
+    private $process;
+
+    /**
      * @var Environment
      */
     private $environment;
-
-    /**
-     * @var ShellInterface
-     */
-    private $shell;
 
     /**
      * @var LoggerInterface
@@ -34,42 +31,26 @@ class DeployStaticContent implements ProcessInterface
     private $logger;
 
     /**
-     * @var Adapter
-     */
-    private $adapter;
-
-    /**
      * @var StaticContentCleaner
      */
     private $staticContentCleaner;
 
     /**
-     * @var DirectoryList
-     */
-    private $directoryList;
-
-    /**
+     * @param ProcessInterface $process
      * @param Environment $environment
-     * @param ShellInterface $shell
      * @param LoggerInterface $logger
-     * @param Adapter $adapter
      * @param StaticContentCleaner $staticContentCleaner
-     * @param DirectoryList $directoryList
      */
     public function __construct(
+        ProcessInterface $process,
         Environment $environment,
-        ShellInterface $shell,
         LoggerInterface $logger,
-        Adapter $adapter,
-        StaticContentCleaner $staticContentCleaner,
-        DirectoryList $directoryList
+        StaticContentCleaner $staticContentCleaner
     ) {
+        $this->process = $process;
         $this->environment = $environment;
-        $this->shell = $shell;
         $this->logger = $logger;
-        $this->adapter = $adapter;
         $this->staticContentCleaner = $staticContentCleaner;
-        $this->directoryList = $directoryList;
     }
 
     /**
@@ -84,95 +65,21 @@ class DeployStaticContent implements ProcessInterface
         $applicationMode = $this->environment->getApplicationMode();
         $this->logger->info('Application mode is ' . $applicationMode);
 
-        if ($applicationMode == Environment::MAGENTO_PRODUCTION_MODE) {
-            /* Workaround for MAGETWO-58594: disable redis cache before running static deploy, re-enable after */
-            if ($this->environment->isDeployStaticContent()) {
-                $this->deployStaticContent();
-            }
+        if ($applicationMode !== Environment::MAGENTO_PRODUCTION_MODE) {
+            return;
         }
-    }
 
-    private function deployStaticContent()
-    {
+        /* Workaround for MAGETWO-58594: disable redis cache before running static deploy, re-enable after */
+        if (!$this->environment->isDeployStaticContent()) {
+            return;
+        }
+
         // Clear old static content if necessary
         if ($this->environment->doCleanStaticFiles()) {
             $this->staticContentCleaner->clean();
         }
+
         $this->logger->info('Generating fresh static content');
-        $this->generateFreshStaticContent();
-    }
-
-    private function generateFreshStaticContent()
-    {
-        $this->shell->execute(
-            'touch ' . $this->directoryList->getMagentoRoot() . '/pub/static/deployed_version.txt'
-        );
-        /* Enable maintenance mode */
-        $this->logger->notice('Enabling Maintenance mode.');
-        $this->shell->execute("php ./bin/magento maintenance:enable {$this->environment->getVerbosityLevel()}");
-
-        /* Generate static assets */
-        $this->logger->notice('Extract locales');
-
-        $excludeThemesOptions = '';
-        if ($this->environment->getStaticDeployExcludeThemes()) {
-            $themes = preg_split("/[,]+/", $this->environment->getStaticDeployExcludeThemes());
-            if (count($themes) > 1) {
-                $excludeThemesOptions = "--exclude-theme=" . implode(' --exclude-theme=', $themes);
-            } elseif (count($themes) === 1) {
-                $excludeThemesOptions = "--exclude-theme=" . $themes[0];
-            }
-        }
-
-        $jobsOption = $this->environment->getStaticDeployThreadsCount()
-            ? "--jobs={$this->environment->getStaticDeployThreadsCount()}"
-            : '';
-
-        $locales = implode(' ', $this->getLocales());
-        $logMessage = $locales ? "Generating static content for locales: $locales" : "Generating static content.";
-        $this->logger->notice($logMessage);
-
-        // @codingStandardsIgnoreStart
-        $this->shell->execute(
-            "php ./bin/magento setup:static-content:deploy  -f $jobsOption $excludeThemesOptions $locales {$this->environment->getVerbosityLevel()}"
-        );
-        // @codingStandardsIgnoreEnd
-
-        /* Disable maintenance mode */
-        $this->shell->execute("php ./bin/magento maintenance:disable {$this->environment->getVerbosityLevel()}");
-        $this->logger->notice('Maintenance mode is disabled.');
-    }
-
-    /**
-     * Gets locales from DB which are set to stores and admin users.
-     * Adds additional default 'en_US' locale to result, if it does't exist yet in defined list.
-     *
-     * @return array List of locales. Returns empty array in case when no locales are defined in DB
-     * ```php
-     * [
-     *     'en_US',
-     *     'fr_FR'
-     * ]
-     * ```
-     */
-    private function getLocales()
-    {
-        $locales = [];
-
-        $query = 'SELECT value FROM core_config_data WHERE path=\'general/locale/code\' '
-            . 'UNION SELECT interface_locale FROM admin_user;';
-        $output = $this->adapter->execute($query);
-
-        if (is_array($output) && count($output) > 1) {
-            //first element should be shifted as it is the name of column
-            array_shift($output);
-            $locales = $output;
-
-            if (!in_array($this->environment->getAdminLocale(), $locales)) {
-                $locales[] = $this->environment->getAdminLocale();
-            }
-        }
-
-        return $locales;
+        $this->process->execute();
     }
 }
