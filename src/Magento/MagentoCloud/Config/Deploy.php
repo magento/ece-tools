fcd836c90b503671673eb76fee8ae5544de8f243
Bohdan Korablov <bkorablov@magento.com>
2017-09-13 17:26:16 +0300

    MAGECLOUD-993: Eliminate FS operations using shell and use File API

--- src/Magento/MagentoCloud/Config/Deploy.php
+++ src/Magento/MagentoCloud/Config/Deploy.php
@@ -8,7 +8,6 @@ namespace Magento\MagentoCloud\Config;
 use Magento\MagentoCloud\DB\Adapter;
 use Magento\MagentoCloud\Filesystem\DirectoryList;
 use Magento\MagentoCloud\Filesystem\Driver\File;
-use Magento\MagentoCloud\Shell\ShellInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -17,19 +16,9 @@ use Psr\Log\LoggerInterface;
 class Deploy
 {
     /**
-     * @var Environment
-     */
-    private $environment;
-
-    /**
      * @var LoggerInterface
      */
     private $logger;
-
-    /**
-     * @var ShellInterface
-     */
-    private $shell;
 
     /**
      * @var Adapter
@@ -47,24 +36,18 @@ class Deploy
     private $directoryList;
 
     /**
-     * @param Environment $environment
      * @param LoggerInterface $logger
-     * @param ShellInterface $shell
      * @param Adapter $adapter
      * @param File $file
      * @param DirectoryList $directoryList
      */
     public function __construct(
-        Environment $environment,
         LoggerInterface $logger,
-        ShellInterface $shell,
         Adapter $adapter,
         File $file,
         DirectoryList $directoryList
     ) {
-        $this->environment = $environment;
         $this->logger = $logger;
-        $this->shell = $shell;
         $this->adapter = $adapter;
         $this->file = $file;
         $this->directoryList = $directoryList;
@@ -73,46 +56,47 @@ class Deploy
     /**
      * Verifies is Magento installed based on install date in env.php
      *
+     * 1. from environment variables check if db exists and has tables
+     * 2. check if core_config_data and setup_module tables exist
+     * 3. check install date
+     *
      * @return bool
      * @throws \Exception
      */
     public function isInstalled()
     {
-        $configFile = $this->getConfigFilePath();
-        $isInstalled = false;
-
-        //1. from environment variables check if db exists and has tables
-        //2. check if core_config_data and setup_module tables exist
-        //3. check install date
-
         $this->logger->info('Checking if db exists and has tables');
         $output = $this->adapter->execute('SHOW TABLES');
 
-        if (is_array($output) && count($output) > 1) {
-            if (!in_array('core_config_data', $output) || !in_array('setup_module', $output)) {
-                throw new \Exception('Missing either core_config_data or setup_module table', 5);
-            } elseif ($this->file->isExists($configFile)) {
-                $data = include $configFile;
-
-                if (isset($data['install']) && isset($data['install']['date'])) {
-                    $this->logger->info('Magento was installed on ' . $data['install']['date']);
-                    $isInstalled = true;
-                } else {
-                    $config['install']['date'] = date('r');
-                    $updatedConfig = '<?php' . "\n" . 'return ' . var_export($config, true) . ';';
-                    $this->file->filePutContents($configFile, $updatedConfig);
-                    $isInstalled = true;
-                }
-            } else {
-                $this->shell->execute('touch ' . $configFile);
-                $config['install']['date'] = date('r');
-                $updatedConfig = '<?php' . "\n" . 'return ' . var_export($config, true) . ';';
-                $this->file->filePutContents($configFile, $updatedConfig);
-                $isInstalled = true;
-            }
+        if (!is_array($output) || count($output) <= 1) {
+            return false;
         }
 
-        return $isInstalled;
+        if (!in_array('core_config_data', $output) || !in_array('setup_module', $output)) {
+            throw new \Exception('Missing either core_config_data or setup_module table', 5);
+        }
+
+        $configFile = $this->getConfigFilePath();
+        if (!$this->file->isExists($configFile)) {
+            $this->updateConfig($configFile);
+            return true;
+        }
+
+        $data = include $configFile;
+        if (isset($data['install']['date'])) {
+            $this->logger->info('Magento was installed on ' . $data['install']['date']);
+            return true;
+        }
+
+        $this->updateConfig($configFile);
+        return true;
+    }
+
+    private function updateConfig(string $configFile)
+    {
+        $config['install']['date'] = date('r');
+        $updatedConfig = '<?php' . "\n" . 'return ' . var_export($config, true) . ';';
+        $this->file->filePutContents($configFile, $updatedConfig);
     }
 
     /**
---
+++ src/Magento/MagentoCloud/Test/Unit/Config/_file/Deploy/with_date/app/etc/env.php
@@ -0,0 +1,7 @@
+<?php
+return array (
+    'install' =>
+        array (
+            'date' => 'Wed, 12 Sep 2017 10:40:30 +0000',
+        ),
+);
