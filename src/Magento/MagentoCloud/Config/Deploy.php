043806ed833d94b780f4cf3032fc86446db1e41b
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2017-09-18 10:28:50 +0300

    MAGECLOUD-1031: Refactor InstallUpdate process

--- src/Magento/MagentoCloud/Config/Deploy.php
+++ src/Magento/MagentoCloud/Config/Deploy.php
@@ -78,31 +78,16 @@ class Deploy
             throw new \Exception('Missing either core_config_data or setup_module table', 5);
         }
 
-        $configPath = $this->getConfigFilePath();
-
-        if (!$this->file->isExists($configPath)) {
-            $this->updateConfig($configPath);
-
-            return true;
+        try {
+            $data = $this->getConfig();
+            if (isset($data['install']['date'])) {
+                $this->logger->info('Magento was installed on ' . $data['install']['date']);
+            }
+        } catch (FileSystemException $e) {
+            $this->write(['install' => ['date' => date('r')]]);
         }
-
-        $data = $this->getConfig();
-        if (isset($data['install']['date'])) {
-            $this->logger->info('Magento was installed on ' . $data['install']['date']);
-
-            return true;
-        }
-
-        $this->updateConfig($configPath);
 
         return true;
-    }
-
-    private function updateConfig(string $configFile)
-    {
-        $config['install']['date'] = date('r');
-        $updatedConfig = '<?php' . "\n" . 'return ' . var_export($config, true) . ';';
-        $this->file->filePutContents($configFile, $updatedConfig);
     }
 
     /**
@@ -131,5 +116,36 @@ class Deploy
         }
 
         return include $configPath;
+    }
+
+    /**
+     * Updates existence configuration.
+     *
+     * @param array $config
+     */
+    public function update(array $config)
+    {
+        try {
+            $oldConfig = $this->getConfig();
+        } catch (FileSystemException $e) {
+            $oldConfig = [];
+        }
+
+        $updatedConfig = array_replace_recursive($oldConfig, $config);
+        $updatedConfig = '<?php' . PHP_EOL . 'return ' . var_export($updatedConfig, true) . ';';
+
+        $this->file->filePutContents($this->getConfigFilePath(), $updatedConfig);
+    }
+
+    /**
+     * Writes given configuration to file.
+     *
+     * @param array $config
+     */
+    public function write(array $config)
+    {
+        $updatedConfig = '<?php' . PHP_EOL . 'return ' . var_export($config, true) . ';';
+
+        $this->file->filePutContents($this->getConfigFilePath(), $updatedConfig);
     }
 }
