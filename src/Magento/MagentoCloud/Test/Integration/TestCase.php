5c96f00f5d71c6c43224d9c3f751afc45ac491dc
Oleh Posyniak <oposyniak@magento.com>
2017-09-21 14:55:10 +0300

    MAGECLOUD-1046: Prepare Travis CI

--- src/Magento/MagentoCloud/Test/Integration/TestCase.php
+++ src/Magento/MagentoCloud/Test/Integration/TestCase.php
@@ -117,8 +117,6 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
         $buildFile = $this->getConfigFile('build_options.ini');
         $deployConfig = (require $this->getConfigFile('environment.php'))['deploy'];
 
-        shell_exec('composer config --list');
-
         if ($deployConfig['type'] === 'git') {
             shell_exec(sprintf(
                 "cd %s && git clone %s . ",
@@ -152,7 +150,6 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
             $buildFile,
             $sandboxDir . '/build_options.ini'
         ));
-        shell_exec('composer config --list');
         shell_exec(sprintf(
             "cd %s && composer install -n",
             $sandboxDir
