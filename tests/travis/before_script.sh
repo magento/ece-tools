95afad33652fcb10a5c05fdeb55df7e95cb13b32
Oleh Posyniak <oposyniak@magento.com>
2017-09-25 17:22:35 +0300

    MAGECLOUD-1062: Eliminate TestCase bootstrap

--- tests/travis/before_script.sh
+++ tests/travis/before_script.sh
@@ -3,26 +3,16 @@
 # Copyright © Magento, Inc. All rights reserved.
 # See COPYING.txt for license details.
 
-echo "==> set"
 set -e
-echo "==> set error handler"
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
-echo "==> switch"
 case $TEST_SUITE in
     integration)
         cd tests/integration/etc
 
-        echo "==> creating mock of composer.json"
         touch composer.json && echo '{}' >> composer.json
-
-        echo "==> setting github token"
         test -n "$GH_TOKEN" && composer config -a -n github-oauth.github.com "$GH_TOKEN" || true
-
-        echo "==> setting repo token"
         composer config -a -n http-basic.repo.magento.com "$REPO_USERNAME" "$REPO_PASSWORD"
-
-        echo "==> setting connect20 token"
         composer config -a -n http-basic.connect20-qa01.magedevteam.com "$CONNECT20_USERNAME" "$CONNECT20_PASSWORD"
 
         cd ../../..
