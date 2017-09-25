486cc86ccca4d1f047f5f10c159134e4c45cb9ec
Oleh Posyniak <oposyniak@magento.com>
2017-09-25 16:22:19 +0300

    MAGECLOUD-1062: Eliminate TestCase bootstrap

--- tests/travis/before_script.sh
+++ tests/travis/before_script.sh
@@ -10,10 +10,16 @@ case $TEST_SUITE in
     integration)
         cd tests/integration/etc
 
+        echo "==> creating mock of composer.json"
         touch composer.json && echo '{}' >> composer.json
 
+        echo "==> setting github token"
         test -n "$GH_TOKEN" && composer config -a -n github-oauth.github.com "$GH_TOKEN" || true
+
+        echo "==> setting repo token"
         composer config -a -n http-basic.repo.magento.com "$REPO_USERNAME" "$REPO_PASSWORD"
+
+        echo "==> setting connect20 token"
         composer config -a -n http-basic.connect20-qa01.magedevteam.com "$CONNECT20_USERNAME" "$CONNECT20_PASSWORD"
 
         cd ../../..
