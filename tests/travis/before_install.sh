4048bac639e448fc08434f01b7aeff7ef4f4a6bb
Oleh Posyniak <oposyniak@magento.com>
2017-09-25 16:29:18 +0300

    MAGECLOUD-1062: Eliminate TestCase bootstrap

--- tests/travis/before_install.sh
+++ tests/travis/before_install.sh
@@ -8,6 +8,8 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 
 case $TEST_SUITE in
     integration)
+        mysql -e 'CREATE DATABASE IF NOT EXISTS integration_tests;'
+
         composer config -a -n github-oauth.github.com "$GH_TOKEN"
         composer config -a -n http-basic.repo.magento.com "$REPO_USERNAME" "$REPO_PASSWORD"
         composer config -a -n http-basic.connect20-qa01.magedevteam.com "$CONNECT20_USERNAME" "$CONNECT20_PASSWORD"
