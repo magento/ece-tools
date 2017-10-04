c958bb924aad49324ba0972d9f60dcf2049756ef
Michael Casey <michaelcasey316@users.noreply.github.com>
2017-10-04 23:37:09 +0000

    Compress html

--- src/Process/Build/CompressStaticContent.php
+++ src/Process/Build/CompressStaticContent.php
@@ -58,7 +58,9 @@ class CompressStaticContent implements ProcessInterface
      * @var string
      */
     private static $compressionCommand
-        = "find pub/static -type f -size +300c -name '*.js' -or -name '*.css' -or -name '*.svg'"
+        = "find pub/static -type f -size +300c"
+        . " -name '*.js' -or -name '*.css' -or -name '*.svg'"
+        . " -or -name '*.html' -or -name '*.htm'"
         . " | xargs -n100 -P16 gzip --keep";
 
     /**
