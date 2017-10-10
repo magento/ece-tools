46010a05f2f86413705a324dcf81898acea20106
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2017-10-10 14:00:42 +0300

    MAGECLOUD-1081: [develop] Disable overwritten of cloud.log file during every redeploy

--- src/Test/Unit/Process/Deploy/PreDeployTest.php
+++ src/Test/Unit/Process/Deploy/PreDeployTest.php
@@ -3,8 +3,12 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\MagentoCloud\Test\Unit\Process\Deploy;
 
+use Magento\MagentoCloud\App\Logger;
+use Magento\MagentoCloud\Filesystem\DirectoryList;
+use Magento\MagentoCloud\Filesystem\Driver\File;
 use Magento\MagentoCloud\Process\Deploy\PreDeploy;
 use Magento\MagentoCloud\Process\ProcessInterface;
 use Magento\MagentoCloud\Package\Manager;
@@ -35,6 +39,16 @@ class PreDeployTest extends TestCase
     private $process;
 
     /**
+     * @var File|Mock
+     */
+    private $fileMock;
+
+    /**
+     * @var DirectoryList|Mock
+     */
+    private $directoryListMock;
+
+    /**
      * @inheritdoc
      */
     protected function setUp()
@@ -44,25 +58,98 @@ class PreDeployTest extends TestCase
         $this->packageManagerMock = $this->createMock(Manager::class);
         $this->processMock = $this->getMockBuilder(ProcessInterface::class)
             ->getMockForAbstractClass();
+        $this->fileMock = $this->createMock(File::class);
+        $this->directoryListMock = $this->createMock(DirectoryList::class);
 
         $this->process = new PreDeploy(
             $this->loggerMock,
             $this->processMock,
-            $this->packageManagerMock
+            $this->packageManagerMock,
+            $this->fileMock,
+            $this->directoryListMock
         );
     }
 
-    public function testExecute()
-    {
+    /**
+     * @param $fileMockFileGetContentsExpects
+     * @param $buildPhaseLogContent
+     * @param $deployLogContent
+     * @param $deployLogPathExists
+     * @param $fileMockFilePutContentsExpects
+     * @param $fileMockCopyExpects
+     * @dataProvider executeDataProvider
+     */
+    public function testExecute(
+        $fileMockFileGetContentsExpects,
+        $buildPhaseLogContent,
+        $deployLogContent,
+        $deployLogPathExists,
+        $fileMockFilePutContentsExpects,
+        $fileMockCopyExpects
+    ) {
+        $magento_root = 'magento_root';
+        $deployLogPath = $magento_root . '/' . Logger::DEPLOY_LOG_PATH;
+        $buildPhaseLogPath = $magento_root . '/' . Logger::BACKUP_BUILD_PHASE_LOG_PATH;
+        $this->directoryListMock->expects($this->once())
+            ->method('getMagentoRoot')
+            ->willReturn($magento_root);
+        $this->fileMock->expects($fileMockFileGetContentsExpects)
+            ->method('fileGetContents')
+            ->withConsecutive($buildPhaseLogPath, $deployLogPath)
+            ->willReturnOnConsecutiveCalls($buildPhaseLogContent, $deployLogContent);
+        $this->fileMock->expects($this->once())
+            ->method('isExists')
+            ->with($deployLogPath)
+            ->willReturn($deployLogPathExists);
+        $this->fileMock->expects($fileMockFilePutContentsExpects)
+            ->method('filePutContents')
+            ->with($deployLogPath, $buildPhaseLogContent, FILE_APPEND);
+        $this->fileMock->expects($fileMockCopyExpects)
+            ->method('copy')
+            ->with($buildPhaseLogPath, $deployLogPath);
+
         $this->packageManagerMock->expects($this->once())
             ->method('getPrettyInfo')
             ->willReturn('(components info)');
-        $this->loggerMock->expects($this->once())
+        $this->loggerMock->expects($this->exactly(2))
             ->method('info')
-            ->with('Starting pre-deploy. (components info)');
+            ->withConsecutive(
+                'Starting deploy.',
+                'Starting pre-deploy. (components info)'
+            );
         $this->processMock->expects($this->once())
             ->method('execute');
 
         $this->process->execute();
+    }
+
+    public function executeDataProvider()
+    {
+        return [
+            [
+                'fileMockFileGetContentsExpects' => $this->once(),
+                'buildPhaseLogContent' => 'the build phase log was not applied',
+                'deployLogContent' => null,
+                'deployLogPathExists' => false,
+                'fileMockFilePutContentsExpects' => $this->never(),
+                'fileMockCopyExpects' => $this->once()
+            ],
+            [
+                'fileMockFileGetContentsExpects' => $this->exactly(2),
+                'buildPhaseLogContent' => 'the build phase log was applied',
+                'deployLogContent' => 'some log the build phase log was applied some log',
+                'deployLogPathExists' => true,
+                'fileMockFilePutContentsExpects' => $this->never(),
+                'fileMockCopyExpects' => $this->once()
+            ],
+            [
+                'fileMockFileGetContentsExpects' => $this->exactly(2),
+                'buildPhaseLogContent' => 'the build phase log was not applied',
+                'deployLogContent' => 'some log the build phase log was applied some log',
+                'deployLogPathExists' => true,
+                'fileMockFilePutContentsExpects' => $this->never(),
+                'fileMockCopyExpects' => $this->once()
+            ]
+        ];
     }
 }
