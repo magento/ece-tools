9dd2273f677d1a9c6824976afe0d2206f1d94ac7
Keith Bentrup <keith@keithbentrup.com>
2017-10-26 20:20:47 -0400

    remove unused var

--- src/App/Container.php
+++ src/App/Container.php
@@ -203,7 +203,7 @@ class Container extends \Illuminate\Container\Container implements ContainerInte
                 true,
                 true
             );
-            $magentoRoot = $this->get(\Magento\MagentoCloud\Filesystem\DirectoryList::class)->getMagentoRoot();
+            // $magentoRoot = $this->get(\Magento\MagentoCloud\Filesystem\DirectoryList::class)->getMagentoRoot();
             $logLevel = getenv('LOG_LEVEL') ?: Logger::DEBUG;
 
             return new Logger($name, [
