9494b4c52516c94e7f016269aac26044ccdffdb8
Oleh Posyniak <oposyniak@magento.com>
2017-10-31 12:10:40 +0200

    MAGECLOUD-1193: Enable strict errors

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+error_reporting(E_ALL);
+date_default_timezone_set('UTC');
+
+require_once __DIR__ . '/autoload.php';
+
+$handler = new \Magento\MagentoCloud\App\ErrorHandler();
+set_error_handler([$handler, 'handler']);
