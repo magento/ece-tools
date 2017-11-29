1830a72425ba69d0de68b0aad712b9da5e7949d8
Oleksandr Shmyheliuk <oleksandr.shmyheliuk@gmail.com>
2017-11-29 13:45:16 +0200

    MAGECLOUD-1343: Drop Stuck Crons During Deployment Process (#103)

--- src/App/Container.php
+++ src/App/Container.php
@@ -96,6 +96,7 @@ class Container extends \Illuminate\Container\Container implements ContainerInte
                         $this->make(DeployProcess\InstallUpdate::class),
                         $this->make(DeployProcess\DeployStaticContent::class),
                         $this->make(DeployProcess\DisableGoogleAnalytics::class),
+                        $this->make(DeployProcess\UnlockCronJobs::class)
                     ],
                 ]);
             });
