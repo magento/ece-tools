cea89568b6fd7fe4cb1a8b2862e63e1cb5b874b4
Jacob Brown <jacob@gnu.org>
2017-12-14 18:10:01 -0600

    MAGECLOUD-1424: Re-factor automatic module enabling (#120)
    * MAGECLOUD-1424: Fixes issue with new modules not being enabled during di:compile by doing this:  saves copy of config.php, enables all modules, then merges old copy back into config.php

---
+++ src/Config/Shared/Writer.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\MagentoCloud\Config\Shared;
+
+use Magento\MagentoCloud\Filesystem\Driver\File;
+use Magento\MagentoCloud\Filesystem\FileList;
+use Magento\MagentoCloud\Filesystem\Writer\WriterInterface;
+
+/**
+ * @inheritdoc
+ */
+class Writer implements WriterInterface
+{
+    /**
+     * @var Reader
+     */
+    private $reader;
+
+    /**
+     * @var File
+     */
+    private $file;
+
+    /**
+     * @var FileList
+     */
+    private $fileList;
+
+    /**
+     * @param Reader $reader ,
+     * @param File $file
+     * @param FileList $fileList
+     */
+    public function __construct(
+        Reader $reader,
+        File $file,
+        FileList $fileList
+    ) {
+        $this->reader = $reader;
+        $this->file = $file;
+        $this->fileList = $fileList;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function create(array $config)
+    {
+        $updatedConfig = '<?php' . PHP_EOL . 'return ' . var_export($config, true) . ';';
+
+        $this->file->filePutContents($this->fileList->getConfig(), $updatedConfig);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function update(array $config)
+    {
+        $this->create(
+            array_replace_recursive($this->reader->read(), $config)
+        );
+    }
+}
