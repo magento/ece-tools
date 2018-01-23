85c78bee549338298956962299763aa69383ed70
Michael Casey <github@mcasey3.com>
2018-01-23 15:29:32 +0000

    Add setup routine to test

--- src/Test/Unit/Config/ScdStrategyCheckerTest.php
+++ src/Test/Unit/Config/ScdStrategyCheckerTest.php
@@ -5,13 +5,45 @@
  */
 namespace Magento\MagentoCloud\Test\Unit\Config;
 
-use PHPUnit\Framework\TestCase;
-use PHPUnit_Framework_MockObject_MockObject as Mock;
+use \Magento\MagentoCloud\Config\ScdStrategyChecker;
+use \Magento\MagentoCloud\App\Logger;
+use \Magento\MagentoCloud\Package\MagentoVersion;
+use \PHPUnit\Framework\TestCase;
+use \PHPUnit_Framework_MockObject_MockObject as Mock;
 
+/**
+ * Class ScdStrategyCheckerTest
+ *
+ * @package Magento\MagentoCloud\Test\Unit\Config
+ */
 class ScdStrategyCheckerTest extends TestCase
 {
+    /**
+     * @var Logger|Mock
+     */
+    private $loggerMock;
+
+    /**
+     * @var MagentoVersion|Mock
+     */
+    private $magentoVersionMock;
+
+    /**
+     * @var ScdStrategyChecker
+     */
+    private $scdStrategyChecker;
+
+    /**
+     *
+     */
     protected function setUp()
     {
-        
+        $this->loggerMock = $this->createMock(Logger::class);
+        $this->magentoVersionMock = $this->createMock(MagentoVersion::class);
+
+        $this->scdStrategyChecker = new ScdStrategyChecker(
+            $this->loggerMock,
+            $this->magentoVersionMock
+        );
     }
 }
