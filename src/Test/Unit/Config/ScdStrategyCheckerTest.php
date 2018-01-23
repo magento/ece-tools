eb55b1ae3e8bdb7d30be4f8d214c6bb189a8db22
Michael Casey <github@mcasey3.com>
2018-01-23 15:23:14 +0000

    Add test for ScdStrategyChecker

---
+++ src/Test/Unit/Config/ScdStrategyCheckerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\MagentoCloud\Test\Unit\Config;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject as Mock;
+
+class ScdStrategyCheckerTest extends TestCase
+{
+    protected function setUp()
+    {
+        
+    }
+}
