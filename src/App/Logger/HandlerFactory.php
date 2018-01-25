8e2895c62c3176cc3e52989543840012d549da79
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2018-01-25 10:00:59 +0200

    MAGECLOUD-1571: Add Possibility to Enable Syslog Handler for Monolog

--- src/App/Logger/HandlerFactory.php
+++ src/App/Logger/HandlerFactory.php
@@ -5,11 +5,14 @@
  */
 namespace Magento\MagentoCloud\App\Logger;
 
+use Monolog\Handler\GelfHandler;
 use Monolog\Handler\SlackHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\NativeMailerHandler;
 use Monolog\Handler\HandlerInterface;
 use Magento\MagentoCloud\Config\Log as LogConfig;
+use Monolog\Handler\SyslogHandler;
+use Monolog\Handler\SyslogUdpHandler;
 
 /**
  * The handler factory.
@@ -20,6 +23,9 @@ class HandlerFactory
     const HANDLER_FILE = 'file';
     const HANDLER_EMAIL = 'email';
     const HANDLER_SLACK = 'slack';
+    const HANDLER_GELF = 'gelf';
+    const HANDLER_SYSLOG = 'syslog';
+    const HANDLER_SYSLOG_UDP = 'syslog_udp';
 
     /**
      * @var LevelResolver
@@ -72,6 +78,34 @@ class HandlerFactory
                     true,
                     null,
                     $minLevel
+                );
+                break;
+            case static::HANDLER_SYSLOG:
+                $handlerInstance = new SyslogHandler(
+                    $configuration->get('ident'),
+                    $configuration->get('facility', LOG_USER),
+                    $minLevel,
+                    $configuration->get('bubble', true),
+                    $configuration->get('logopts', LOG_PID)
+                );
+                break;
+            case static::HANDLER_SYSLOG_UDP:
+                $handlerInstance = new SyslogUdpHandler(
+                    $configuration->get('host'),
+                    $configuration->get('port'),
+                    $configuration->get('facility', LOG_USER),
+                    $minLevel,
+                    $configuration->get('bubble', true),
+                    $configuration->get('ident', 'php')
+                );
+                break;
+            case static::HANDLER_GELF:
+                $publisher = new Publisher();
+
+                $handlerInstance = new GelfHandler(
+                    $publisher,
+                    $minLevel,
+                    $configuration->get('bubble', true)
                 );
                 break;
             default:
