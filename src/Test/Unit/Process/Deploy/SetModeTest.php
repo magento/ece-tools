785570691deb641bbebf8416f041985de4bd0fe5
Oleh Posyniak <oleg.posyniak@gmail.com>
2018-01-31 14:11:19 +0200

    MAGECLOUD-1615: Remove DeployProcess\SetMode (#172)

--- src/Test/Unit/Process/Deploy/SetModeTest.php
+++ src/Test/Unit/Process/Deploy/SetModeTest.php
@@ -5,11 +5,8 @@
  */
 namespace Magento\MagentoCloud\Test\Unit\Process\Deploy;
 
-use Magento\MagentoCloud\Config\Deploy\Writer as DeployConfigWriter;
-use Magento\MagentoCloud\Config\Environment;
-use Magento\MagentoCloud\Config\Stage\DeployInterface;
+use Magento\MagentoCloud\Config\Deploy\Writer;
 use Magento\MagentoCloud\Process\Deploy\SetMode;
-use Magento\MagentoCloud\Shell\ShellInterface;
 use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject as Mock;
 use Psr\Log\LoggerInterface;
@@ -25,89 +22,37 @@ class SetModeTest extends TestCase
     private $process;
 
     /**
-     * @var Environment|Mock
-     */
-    private $environmentMock;
-
-    /**
-     * @var ShellInterface|Mock
-     */
-    private $shellMock;
-
-    /**
      * @var LoggerInterface|Mock
      */
     private $loggerMock;
 
     /**
-     * @var DeployConfigWriter|Mock
+     * @var Writer|Mock
      */
-    private $deployConfigWriterMock;
-
-    /**
-     * @var DeployInterface|Mock
-     */
-    private $stageConfigMock;
+    private $writer;
 
     /**
      * @inheritdoc
      */
     protected function setUp()
     {
-        $this->environmentMock = $this->createMock(Environment::class);
-        $this->shellMock = $this->getMockForAbstractClass(ShellInterface::class);
         $this->loggerMock = $this->getMockForAbstractClass(LoggerInterface::class);
-        $this->deployConfigWriterMock = $this->createMock(DeployConfigWriter::class);
-        $this->stageConfigMock = $this->getMockForAbstractClass(DeployInterface::class);
+        $this->writer = $this->createMock(Writer::class);
 
         $this->process = new SetMode(
-            $this->environmentMock,
             $this->loggerMock,
-            $this->shellMock,
-            $this->deployConfigWriterMock,
-            $this->stageConfigMock
+            $this->writer
         );
     }
 
     public function testExecute()
     {
-        $mode = Environment::MAGENTO_PRODUCTION_MODE;
-
-        $this->environmentMock->expects($this->once())
-            ->method('getApplicationMode')
-            ->willReturn($mode);
         $this->loggerMock->expects($this->once())
             ->method('info')
-            ->willReturn(sprintf("Set Magento application mode to '%s'", $mode));
-        $this->deployConfigWriterMock->expects($this->once())
+            ->willReturn("Set Magento application mode to 'production'");
+        $this->writer->expects($this->once())
             ->method('update')
             ->with(['MAGE_MODE' => 'production']);
-
-        $this->process->execute();
-    }
-
-    public function testExecuteDeveloperMode()
-    {
-        $mode = Environment::MAGENTO_DEVELOPER_MODE;
-        $verbosity = '-vvv';
-
-        $this->environmentMock->expects($this->once())
-            ->method('getApplicationMode')
-            ->willReturn($mode);
-        $this->stageConfigMock->expects($this->once())
-            ->method('get')
-            ->with(DeployInterface::VAR_VERBOSE_COMMANDS)
-            ->willReturn($verbosity);
-        $this->loggerMock->expects($this->once())
-            ->method('info')
-            ->willReturn(sprintf("Set Magento application mode to '%s'", $mode));
-        $this->shellMock->expects($this->once())
-            ->method('execute')
-            ->with(sprintf(
-                "php ./bin/magento deploy:mode:set %s %s",
-                $mode,
-                $verbosity
-            ));
 
         $this->process->execute();
     }
