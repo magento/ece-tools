1e0b68dfe7e1b4b2a2c9726f48c21940fd0986be
Oleh Posyniak <oposyniak@magento.com>
2018-02-06 11:24:36 +0200

    Merge branches 'MAGECLOUD-1086' and 'develop' of https://github.com/magento/ece-tools into MAGECLOUD-1086
    # Conflicts:
    #	src/Process/ConfigDump/Export.php
    #	src/Test/Unit/Process/ConfigDump/ExportTest.php

--- src/Test/Unit/Process/Deploy/InstallUpdate/ConfigUpdate/CronConsumersRunnerTest.php
+++ src/Test/Unit/Process/Deploy/InstallUpdate/ConfigUpdate/CronConsumersRunnerTest.php
@@ -224,11 +224,9 @@ class CronConsumersRunnerTest extends TestCase
             ->method('isGreaterOrEqual')
             ->with('2.2')
             ->willReturn(false);
-
         $this->magentoVersionMock->expects($this->once())
             ->method('getVersion')
             ->willReturn('2.1.7');
-        
         $this->loggerMock->expects($this->once())
             ->method('info')
             ->with('Updating cron consumer runner is not supported in Magento 2.1.7, skipping.');
@@ -237,7 +235,7 @@ class CronConsumersRunnerTest extends TestCase
         $this->stageConfigMock->expects($this->never())
             ->method('get');
         $this->configWriterMock->expects($this->never())
-            ->method('write');
+            ->method('create');
         
         $this->cronConsumersRunner->execute();
     }
