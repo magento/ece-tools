bf1bb819f664f4f618eaedfbeb0c525843bb7df0
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-02-19 11:22:33 +0200

    MAGECLOUD-1621: Remove view_preprocessed to Generate on Fly

--- src/Test/Unit/Config/Stage/BuildTest.php
+++ src/Test/Unit/Config/Stage/BuildTest.php
@@ -3,7 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\MagentoCloud\Test\Unit\Config\Stage;
 
 use Magento\MagentoCloud\Config\Stage\Build;
