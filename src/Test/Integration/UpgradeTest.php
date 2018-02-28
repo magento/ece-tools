ce22a14b700d3004acd20c50bd40da1e8bbf4a75
Oleh Posyniak <oposyniak@magento.com>
2018-02-27 21:21:47 -0600

    MAGECLOUD-1724: Add 2.1 Acceptance test

--- src/Test/Integration/UpgradeTest.php
+++ src/Test/Integration/UpgradeTest.php
@@ -81,8 +81,8 @@ class UpgradeTest extends AbstractTest
     public function defaultDataProvider(): array
     {
         return [
-            ['^2.1.0', '2.2.0'],
-            ['2.2.0', '^2.2.0'],
+            ['2.1.*', '2.2.0'],
+            ['2.2.0', '2.2.*'],
         ];
     }
 
