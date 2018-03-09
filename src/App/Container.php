643f8530488d3c24ba732865bea0181fdcabd43f
Oleh Posyniak <oleg.posyniak@gmail.com>
2018-03-09 14:30:12 -0600

    MAGECLOUD-1788: Configure post_deploy Hook to Clean Cache & Run Cache-Warming (#203)

--- src/App/Container.php
+++ src/App/Container.php
@@ -119,6 +119,7 @@ class Container implements ContainerInterface
         $this->container->singleton(DirectoryCopier\StrategyFactory::class);
         $this->container->singleton(\Magento\MagentoCloud\Config\Stage\Build::class);
         $this->container->singleton(\Magento\MagentoCloud\Config\Stage\Deploy::class);
+        $this->container->singleton(\Magento\MagentoCloud\Config\Stage\PostDeploy::class);
         $this->container->singleton(\Magento\MagentoCloud\Config\RepositoryFactory::class);
         $this->container->singleton(
             \Magento\MagentoCloud\Config\Stage\BuildInterface::class,
@@ -127,6 +128,10 @@ class Container implements ContainerInterface
         $this->container->singleton(
             \Magento\MagentoCloud\Config\Stage\DeployInterface::class,
             \Magento\MagentoCloud\Config\Stage\Deploy::class
+        );
+        $this->container->singleton(
+            \Magento\MagentoCloud\Config\Stage\PostDeployInterface::class,
+            \Magento\MagentoCloud\Config\Stage\PostDeploy::class
         );
         $this->container->singleton(\Magento\MagentoCloud\Shell\UtilityManager::class);
         /**
@@ -201,6 +206,13 @@ class Container implements ContainerInterface
                         $this->container->make(DeployProcess\CompressStaticContent::class),
                         $this->container->make(DeployProcess\DisableGoogleAnalytics::class),
                         $this->container->make(DeployProcess\UnlockCronJobs::class),
+                        $this->container->make(\Magento\MagentoCloud\Process\ValidateConfiguration::class, [
+                            'validators' => [
+                                ValidatorInterface::LEVEL_WARNING => [
+                                    $this->container->make(ConfigValidator\Deploy\PostDeploy::class),
+                                ],
+                            ],
+                        ]),
                         /**
                          * Remove this line after implementation post-deploy hook
                          */
@@ -209,7 +221,7 @@ class Container implements ContainerInterface
                          * Cache clean process must remain the last one in deploy chain.
                          * Do not add any processes after it.
                          */
-                        $this->container->make(Process\CleanCache::class),
+                        $this->container->make(PostDeployProcess\CleanCache::class),
                     ],
                 ]);
             });
@@ -343,6 +355,7 @@ class Container implements ContainerInterface
                     'processes' => [
                         $this->container->make(PostDeployProcess\Backup::class),
                         $this->container->make(PostDeployProcess\CleanCache::class),
+                        $this->container->make(PostDeployProcess\WarmUp::class),
                     ],
                 ]);
             });
