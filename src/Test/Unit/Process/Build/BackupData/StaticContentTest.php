ef3e13f24e02ef2a7cdb3823978e9d7a0ee75c24
Billy Gilbert <bgilbert@magento.com>
2018-03-13 12:35:59 -0500

    MAGECLOUD-1842 Moving static content to init directory instead of copying

--- src/Test/Unit/Process/Build/BackupData/StaticContentTest.php
+++ src/Test/Unit/Process/Build/BackupData/StaticContentTest.php
@@ -12,6 +12,7 @@ use Magento\MagentoCloud\Filesystem\Driver\File;
 use Magento\MagentoCloud\Filesystem\Flag\Manager as FlagManager;
 use Psr\Log\LoggerInterface;
 use Magento\MagentoCloud\Process\Build\BackupData\StaticContent;
+use Magento\MagentoCloud\Filesystem\FileSystemException;
 
 /**
  * @inheritdoc
@@ -114,8 +115,11 @@ class StaticContentTest extends TestCase
             ->with($this->initPubStaticPath);
 
         $this->fileMock->expects($this->once())
-            ->method('copyDirectory')
+            ->method('rename')
             ->with($this->originalPubStaticPath, $this->initPubStaticPath);
+
+        $this->fileMock->expects($this->never())
+            ->method('copyDirectory');
 
         $this->process->execute();
     }
@@ -155,8 +159,11 @@ class StaticContentTest extends TestCase
             ->with($this->initPubStaticPath);
 
         $this->fileMock->expects($this->once())
-            ->method('copyDirectory')
+            ->method('rename')
             ->with($this->originalPubStaticPath, $this->initPubStaticPath);
+
+        $this->fileMock->expects($this->never())
+            ->method('copyDirectory');
 
         $this->process->execute();
     }
@@ -186,6 +193,52 @@ class StaticContentTest extends TestCase
         $this->loggerMock->expects($this->once())
             ->method('info')
             ->with('SCD not performed during build');
+
+        $this->process->execute();
+    }
+
+    public function testRenameFails()
+    {
+        $this->flagManagerMock->expects($this->once())
+            ->method('exists')
+            ->with(FlagManager::FLAG_STATIC_CONTENT_DEPLOY_IN_BUILD)
+            ->willReturn(true);
+
+        $this->directoryListMock->expects($this->exactly(2))
+            ->method('getPath')
+            ->willReturnMap([
+                [DirectoryList::DIR_STATIC, false, $this->originalPubStaticPath],
+                [DirectoryList::DIR_INIT, false, $this->rootInitDir]
+            ]);
+
+        $this->fileMock->expects($this->once())
+            ->method('isExists')
+            ->with($this->initPubStaticPath)
+            ->willReturn(false);
+
+        $this->loggerMock->expects($this->exactly(2))
+            ->method('info')
+            ->withConsecutive(
+                ['Create ./init/pub/static'],
+                ['Moving static content to init directory']
+            );
+
+        $this->fileMock->expects($this->never())
+            ->method('backgroundClearDirectory')
+            ->with($this->initPubStaticPath);
+
+        $this->fileMock->expects($this->once())
+            ->method('createDirectory')
+            ->with($this->initPubStaticPath);
+
+        $this->fileMock->expects($this->once())
+            ->method('rename')
+            ->with($this->originalPubStaticPath, $this->initPubStaticPath)
+            ->will($this->throwException(new FileSystemException()));
+
+        $this->fileMock->expects($this->once())
+            ->method('copyDirectory')
+            ->with($this->originalPubStaticPath, $this->initPubStaticPath);
 
         $this->process->execute();
     }
