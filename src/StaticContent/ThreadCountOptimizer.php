d2f5c91edc1c01c83a71301b2d5de1c65d0b3d51
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-03-13 19:51:24 +0200

    MAGECLOUD-1822: Remove workaround with compact strategy and more than 1 job after 2.2.4 release

--- src/StaticContent/ThreadCountOptimizer.php
+++ src/StaticContent/ThreadCountOptimizer.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\MagentoCloud\StaticContent;
 
+use Magento\MagentoCloud\Package\MagentoVersion;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -12,6 +13,7 @@ use Psr\Log\LoggerInterface;
  */
 class ThreadCountOptimizer
 {
+    const FORCE_CHANGE_THREAD_COUNT_COMPACT_STRATEGY_VERSION_CONSTRAINT = '<2.2.4';
     /**
      * @var string
      */
@@ -30,11 +32,18 @@ class ThreadCountOptimizer
     private $logger;
 
     /**
-     * @param LoggerInterface $logger
+     * @var MagentoVersion
      */
-    public function __construct(LoggerInterface $logger)
+    private $magentoVersion;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param MagentoVersion $magentoVersion
+     */
+    public function __construct(LoggerInterface $logger, MagentoVersion $magentoVersion)
     {
         $this->logger = $logger;
+        $this->magentoVersion = $magentoVersion;
     }
 
     /**
@@ -46,7 +55,9 @@ class ThreadCountOptimizer
      */
     public function optimize(int $threads, string $strategy): int
     {
-        if ($strategy === self::STRATEGY_COMPACT) {
+        if ($strategy === self::STRATEGY_COMPACT
+            && $this->magentoVersion->satisfies(self::FORCE_CHANGE_THREAD_COUNT_COMPACT_STRATEGY_VERSION_CONSTRAINT)
+        ) {
             if ($threads !== self::THREAD_COUNT_COMPACT_STRATEGY) {
                 $this->logger->notice(
                     'Threads count was forced to 1 as compact strategy can\'t be run with more than one job'
