7389ab909badd7af934d29ef88996efc5e5607c4
Oleh Posyniak <oposyniak@magento.com>
2018-04-03 13:23:40 -0500

    MAGECLOUD-1829: Move Docker configuration from ECE-Tools to Hub

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -6,22 +6,23 @@
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
-BASH="docker-compose -f docker-compose-$PHP.yml run cli bash"
+BASH="docker-compose -f ./docker-compose-${PHP}.yml run cli bash"
+ECE_DIR="/var/www/magento"
 
 case $TEST_SUITE in
     static-unit)
-        $BASH -c "/var/www/magento/vendor/bin/phpcs /var/www/magento/src --standard=/var/www/magento/tests/static/phpcs-ruleset.xml -p -n"
-        $BASH -c "/var/www/magento/vendor/bin/phpmd /var/www/magento/src xml /var/www/magento/tests/static/phpmd-ruleset.xml"
-        $BASH -c "/var/www/magento/vendor/bin/phpunit --configuration /var/www/magento/tests/unit --coverage-clover /var/www/magento/tests/unit/tmp/clover.xml && php /var/www/magento/tests/unit/code-coverage.php /var/www/magento/tests/unit/tmp/clover.xml"
-        $BASH -c "/var/www/magento/vendor/bin/phpunit --configuration /var/www/magento/tests/unit"
+        $BASH -c "${ECE_DIR}/vendor/bin/phpcs ${ECE_DIR}/src --standard=${ECE_DIR}/tests/static/phpcs-ruleset.xml -p -n"
+        $BASH -c "${ECE_DIR}/vendor/bin/phpmd ${ECE_DIR}/src xml ${ECE_DIR}/tests/static/phpmd-ruleset.xml"
+        $BASH -c "${ECE_DIR}/vendor/bin/phpunit --configuration ${ECE_DIR}/tests/unit --coverage-clover ${ECE_DIR}/tests/unit/tmp/clover.xml && php ${ECE_DIR}/tests/unit/code-coverage.php ${ECE_DIR}/tests/unit/tmp/clover.xml"
+        $BASH -c "${ECE_DIR}/vendor/bin/phpunit --configuration ${ECE_DIR}/tests/unit"
         ;;
     integration)
         case $PHP in
             7.0)
-                $BASH -c "/var/www/magento/vendor/bin/phpunit --group php70 --verbose --configuration /var/www/magento/tests/integration"
+                $BASH -c "${ECE_DIR}/vendor/bin/phpunit --group php70 --verbose --configuration ${ECE_DIR}/tests/integration"
                 ;;
             7.1)
-                $BASH -c "/var/www/magento/vendor/bin/phpunit --exclude-group php70 --verbose --configuration /var/www/magento/tests/integration"
+                $BASH -c "${ECE_DIR}/vendor/bin/phpunit --exclude-group php70 --verbose --configuration ${ECE_DIR}/tests/integration"
                 ;;
         esac
         ;;
