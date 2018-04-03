00729f73bc6a07c5ec8dfb0b4524ad0cb56f384b
Oleh Posyniak <oposyniak@magento.com>
2018-04-03 14:03:25 -0500

    MAGECLOUD-1829: Move Docker configuration from ECE-Tools to Hub

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -7,22 +7,21 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 BASH="docker-compose -f ./docker-compose-${PHP}.yml run cli bash"
-ECE_DIR="/var/www/magento"
 
 case $TEST_SUITE in
     static-unit)
-        $BASH -c "${ECE_DIR}/vendor/bin/phpcs ${ECE_DIR}/src --standard=${ECE_DIR}/tests/static/phpcs-ruleset.xml -p -n"
-        $BASH -c "${ECE_DIR}/vendor/bin/phpmd ${ECE_DIR}/src xml ${ECE_DIR}/tests/static/phpmd-ruleset.xml"
-        $BASH -c "${ECE_DIR}/vendor/bin/phpunit --configuration ${ECE_DIR}/tests/unit --coverage-clover ${ECE_DIR}/tests/unit/tmp/clover.xml && php ${ECE_DIR}/tests/unit/code-coverage.php ${ECE_DIR}/tests/unit/tmp/clover.xml"
-        $BASH -c "${ECE_DIR}/vendor/bin/phpunit --configuration ${ECE_DIR}/tests/unit"
+        $BASH -c "${DIR_TOOLS}/vendor/bin/phpcs ${DIR_TOOLS}/src --standard=${DIR_TOOLS}/tests/static/phpcs-ruleset.xml -p -n"
+        $BASH -c "${DIR_TOOLS}/vendor/bin/phpmd ${DIR_TOOLS}/src xml ${DIR_TOOLS}/tests/static/phpmd-ruleset.xml"
+        $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --configuration ${DIR_TOOLS}/tests/unit --coverage-clover ${DIR_TOOLS}/tests/unit/tmp/clover.xml && php ${DIR_TOOLS}/tests/unit/code-coverage.php ${DIR_TOOLS}/tests/unit/tmp/clover.xml"
+        $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --configuration ${DIR_TOOLS}/tests/unit"
         ;;
     integration)
         case $PHP in
             7.0)
-                $BASH -c "${ECE_DIR}/vendor/bin/phpunit --group php70 --verbose --configuration ${ECE_DIR}/tests/integration"
+                $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --group php70 --verbose --configuration ${DIR_TOOLS}/tests/integration"
                 ;;
             7.1)
-                $BASH -c "${ECE_DIR}/vendor/bin/phpunit --exclude-group php70 --verbose --configuration ${ECE_DIR}/tests/integration"
+                $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --exclude-group php70 --verbose --configuration ${DIR_TOOLS}/tests/integration"
                 ;;
         esac
         ;;
