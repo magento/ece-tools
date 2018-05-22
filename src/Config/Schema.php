86dd9982f117dc3b15b2963e12e88293fa9343e9
Billy Gilbert <billy@billygilbert.net>
2018-05-22 01:49:25 -0500

    MAGECLOUD-2218: Can't specify variable DEPLOYED_MAGENTO_VERSION_FROM_GIT (#263)
    * MAGECLOUD-2218: Can't specify variable DEPLOYED_MAGENTO_VERSION_FROM_GIT

--- src/Config/Schema.php
+++ src/Config/Schema.php
@@ -174,12 +174,12 @@ class Schema
                 ],
             ],
             StageConfigInterface::VAR_DEPLOYED_MAGENTO_VERSION_FROM_GIT => [
-                self::SCHEMA_TYPE => ['array', 'boolean'],
+                self::SCHEMA_TYPE => ['string'],
                 self::SCHEMA_STAGE => [
                     StageConfigInterface::STAGE_GLOBAL
                 ],
                 self::SCHEMA_DEFAULT_VALUE => [
-                    StageConfigInterface::STAGE_GLOBAL => false,
+                    StageConfigInterface::STAGE_GLOBAL => '',
                 ],
             ],
             StageConfigInterface::VAR_DEPLOY_FROM_GIT_OPTIONS => [
