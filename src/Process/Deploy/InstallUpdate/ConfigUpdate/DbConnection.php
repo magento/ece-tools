e78cd475ceae811d76654da24a852aeb99186b4c
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2018-05-22 10:53:25 +0300

    MAGECLOUD-2047: Add Variable to Configure Database Connection Settings (#257)

--- src/Process/Deploy/InstallUpdate/ConfigUpdate/DbConnection.php
+++ src/Process/Deploy/InstallUpdate/ConfigUpdate/DbConnection.php
@@ -5,12 +5,12 @@
  */
 namespace Magento\MagentoCloud\Process\Deploy\InstallUpdate\ConfigUpdate;
 
-use Magento\MagentoCloud\Config\Environment;
-use Magento\MagentoCloud\Config\Stage\DeployInterface;
-use Magento\MagentoCloud\DB\Data\ConnectionInterface;
-use Magento\MagentoCloud\Process\ProcessInterface;
 use Magento\MagentoCloud\Config\Deploy\Reader as ConfigReader;
 use Magento\MagentoCloud\Config\Deploy\Writer as ConfigWriter;
+use Magento\MagentoCloud\Config\Stage\DeployInterface;
+use Magento\MagentoCloud\Config\StageConfigInterface;
+use Magento\MagentoCloud\Process\Deploy\InstallUpdate\ConfigUpdate\Db\Config;
+use Magento\MagentoCloud\Process\ProcessInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -18,11 +18,6 @@ use Psr\Log\LoggerInterface;
  */
 class DbConnection implements ProcessInterface
 {
-    /**
-     * @var Environment
-     */
-    private $environment;
-
     /**
      * @var LoggerInterface
      */
@@ -39,36 +34,23 @@ class DbConnection implements ProcessInterface
     private $configReader;
 
     /**
-     * Data of read connection
-     * @var ConnectionInterface
+     * @var Config
      */
-    private $readConnection;
+    private $dbConfig;
 
     /**
-     * Configurations for deploy phase
-     * @var DeployInterface
-     */
-    private $deployConfig;
-
-    /**
-     * @param Environment $environment
-     * @param ConnectionInterface $readConnection
-     * @param DeployInterface $deployConfig
+     * @param Config $dbConfig
      * @param ConfigWriter $configWriter
      * @param ConfigReader $configReader
      * @param LoggerInterface $logger
      */
     public function __construct(
-        Environment $environment,
-        ConnectionInterface $readConnection,
-        DeployInterface $deployConfig,
+        Config $dbConfig,
         ConfigWriter $configWriter,
         ConfigReader $configReader,
         LoggerInterface $logger
     ) {
-        $this->environment = $environment;
-        $this->readConnection = $readConnection;
-        $this->deployConfig = $deployConfig;
+        $this->dbConfig = $dbConfig;
         $this->configWriter = $configWriter;
         $this->configReader = $configReader;
         $this->logger = $logger;
@@ -79,68 +61,16 @@ class DbConnection implements ProcessInterface
      */
     public function execute()
     {
-        $this->logger->info('Updating env.php DB connection configuration.');
-
-        $mainConnectionData = [
-            'username' => $this->environment->getDbUser(),
-            'host' => $this->environment->getDbHost(),
-            'dbname' => $this->environment->getDbName(),
-            'password' => $this->environment->getDbPassword(),
-        ];
-
-        $DbConfig = [
-            'db' => [
-                'connection' => [
-                    'default' => $mainConnectionData,
-                    'indexer' => $mainConnectionData,
-                ],
-            ],
-            'resource' => [
-                'default_setup' => [
-                    'connection' => 'default',
-                ],
-            ],
-        ];
-
         $config = $this->configReader->read();
-        $config = array_replace_recursive($config, $DbConfig);
 
-        $slaveConnectionData = $this->getSlaveConnection();
-
-        if (!$slaveConnectionData) {
-            unset($config['db']['slave_connection']);
-        } else {
-            $config['db']['slave_connection']['default'] = $slaveConnectionData;
-        }
+        $this->logger->info('Updating env.php DB connection configuration.');
+        $config['db'] = $this->dbConfig->get();
+        $config['resource'] = [
+            'default_setup' => [
+                'connection' => 'default',
+            ],
+        ];
 
         $this->configWriter->create($config);
-    }
-
-    /**
-     * Returns mysql read connection if MYSQL_USE_SLAVE_CONNECTION is enabled otherwise returns empty array.
-     *
-     * @return array
-     */
-    private function getSlaveConnection(): array
-    {
-        $slaveConnection = [];
-        if ($this->deployConfig->get(DeployInterface::VAR_MYSQL_USE_SLAVE_CONNECTION)
-            && $this->readConnection->getHost()
-        ) {
-            $this->logger->info('Set DB slave connection.');
-
-            $slaveConnection = [
-                'host' => $this->readConnection->getHost() . ':' . $this->readConnection->getPort(),
-                'username' => $this->readConnection->getUser(),
-                'dbname' => $this->readConnection->getDBName(),
-                'password' => $this->readConnection->getPassword(),
-                'model' => 'mysql4',
-                'engine' => 'innodb',
-                'initStatements' => 'SET NAMES utf8;',
-                'active' => '1',
-            ];
-        }
-        
-        return $slaveConnection;
     }
 }
--- src/Test/Unit/Process/Deploy/InstallUpdate/ConfigUpdate/DbConnectionTest.php
+++ src/Test/Unit/Process/Deploy/InstallUpdate/ConfigUpdate/DbConnectionTest.php
@@ -5,31 +5,19 @@
  */
 namespace Magento\MagentoCloud\Test\Unit\Process\Deploy\InstallUpdate\ConfigUpdate;
 
-use Magento\MagentoCloud\Config\Stage\DeployInterface;
-use Magento\MagentoCloud\DB\Data\ReadConnection;
-use Magento\MagentoCloud\Process\Deploy\InstallUpdate\ConfigUpdate\DbConnection;
-use PHPUnit\Framework\TestCase;
-use Magento\MagentoCloud\Config\Environment;
 use Magento\MagentoCloud\Config\Deploy\Reader as ConfigReader;
 use Magento\MagentoCloud\Config\Deploy\Writer as ConfigWriter;
-use Psr\Log\LoggerInterface;
+use Magento\MagentoCloud\Process\Deploy\InstallUpdate\ConfigUpdate\Db\Config;
+use Magento\MagentoCloud\Process\Deploy\InstallUpdate\ConfigUpdate\DbConnection;
+use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject as Mock;
+use Psr\Log\LoggerInterface;
 
 /**
  * @inheritdoc
  */
 class DbConnectionTest extends TestCase
 {
-    /**
-     * @var Environment|Mock
-     */
-    private $environmentMock;
-
-    /**
-     * @var DeployInterface|Mock
-     */
-    private $deployConfigMock;
-
     /**
      * @var LoggerInterface|Mock
      */
@@ -46,38 +34,27 @@ class DbConnectionTest extends TestCase
     private $configReaderMock;
 
     /**
+     * @var Config|Mock
+     */
+    private $dbConfigMock;
+
+    /**
+     * @var DbConnection
+     */
+    private $process;
+
+    /**
      * @inheritdoc
      */
     protected function setUp()
     {
-        $this->environmentMock = $this->getMockBuilder(Environment::class)
-            ->setMethods(['getRelationships'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->deployConfigMock = $this->getMockBuilder(DeployInterface::class)
-            ->getMockForAbstractClass();
-        $this->loggerMock = $this->getMockBuilder(LoggerInterface::class)
-            ->getMockForAbstractClass();
+        $this->dbConfigMock = $this->createMock(Config::class);
         $this->configWriterMock = $this->createMock(ConfigWriter::class);
         $this->configReaderMock = $this->createMock(ConfigReader::class);
-    }
+        $this->loggerMock = $this->getMockForAbstractClass(LoggerInterface::class);
 
-    /**
-     * @param $relationships
-     * @return DbConnection
-     */
-    public function getProcess($relationships)
-    {
-        $this->environmentMock->expects($this->any())
-            ->method('getRelationships')
-            ->willReturn($relationships);
-
-        $readConnection = new ReadConnection($this->environmentMock);
-
-        return new DbConnection(
-            $this->environmentMock,
-            $readConnection,
-            $this->deployConfigMock,
+        $this->process = new DbConnection(
+            $this->dbConfigMock,
             $this->configWriterMock,
             $this->configReaderMock,
             $this->loggerMock
@@ -85,211 +62,42 @@ class DbConnectionTest extends TestCase
     }
 
     /**
-     * @param $relationshipData array
-     * @param $configData array
-     * @param $setSlave boolean
-     * @param $resultConfigData array
-     *
-     * @dataProvider executeDataProvider
+     * @inheritdoc
      */
-    public function testExecute($relationshipData, $configData, $setSlave, $resultConfigData)
+    public function testExecute()
     {
+        $this->dbConfigMock->expects($this->once())
+            ->method('get')
+            ->willReturn(['connection' => [
+                'default' => ['host' => 'some.host']
+            ]]);
+        $this->loggerMock->expects($this->once())
+            ->method('info')
+            ->with('Updating env.php DB connection configuration.');
         $this->configReaderMock->expects($this->once())
             ->method('read')
-            ->willReturn($configData);
-
-        $this->deployConfigMock->expects($this->once())
-            ->method('get')
-            ->with(DeployInterface::VAR_MYSQL_USE_SLAVE_CONNECTION)
-            ->willReturn($setSlave);
-
+            ->willReturn([
+                'db' => [
+                    'connection' => [
+                        'default' => ['host' => 'custom.host']
+                    ],
+                ],
+            ]);
         $this->configWriterMock->expects($this->once())
             ->method('create')
-            ->with($resultConfigData);
-
-        $process = $this->getProcess($relationshipData);
-
-        $process->execute();
-    }
-
-    /**
-     * Data provider for testExecute.
-     *
-     * Return data for 2 parameters:
-     * 1 - relationships data
-     * 2 - previous config data
-     * 3 - value for VAR_MYSQL_USE_SLAVE_CONNECTION variable
-     * 4 - result of updated config data for configuration file
-     *
-     * @return array
-     *
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
-    public function executeDataProvider()
-    {
-        $relationships = [
-            'database' => [
-                0 => [
-                    'host' => 'localhost',
-                    'port' => '3306',
-                    'path' => 'magento',
-                    'username' => 'user',
-                    'password' => 'password',
-                ]
-            ],
-            'database-slave' => [
-                0 => [
-                    'host' => 'slave.host',
-                    'port' => 'slave.port',
-                    'path' => 'slave.name',
-                    'username' => 'slave.user',
-                    'password' => 'slave.pswd',
-                ]
-            ],
-        ];
-        $relationshipsWithoutSlave = $relationships;
-        $relationshipsWithoutSlave['database-slave'][0] = [];
-
-        $masterConnection = [
-            'username' => 'user',
-            'host' => 'localhost',
-            'dbname' => 'magento',
-            'password' => 'password'
-        ];
-
-        $slaveConnection = [
-            'host' => 'slave.host:slave.port',
-            'username' => 'slave.user',
-            'dbname' => 'slave.name',
-            'password' => 'slave.pswd',
-            'model' => 'mysql4',
-            'engine' => 'innodb',
-            'initStatements' => 'SET NAMES utf8;',
-            'active' => '1',
-        ];
-
-        $baseResult = [
-            'db' => [
-                'connection' => [
-                    'default' => $masterConnection,
-                    'indexer' => $masterConnection,
-                ]
-            ],
-            'resource' => [
-                'default_setup' => [
-                    'connection' => 'default',
-                ],
-            ],
-        ];
-
-        $resultWithSlave = [
-            'db' => [
-                'connection' => [
-                    'default' => $masterConnection,
-                    'indexer' => $masterConnection,
-                ],
-                'slave_connection' => [
-                    'default' => $slaveConnection,
-                ]
-            ],
-            'resource' => [
-                'default_setup' => [
-                    'connection' => 'default',
-                ],
-            ],
-        ];
-
-        $ownConnection = [
-            'db' => [
-                'own_connection' => ['some connection data'],
-            ]
-        ];
-
-        return [
-            [
-                $relationships,
-                [],
-                false,
-                $baseResult
-            ],
-            [
-                $relationships,
-                [
-                    'db' => [
-                        'connection' => [
-                            'default' => 'some connection data'
-                        ],
+            ->with([
+                'db' => [
+                    'connection' => [
+                        'default' => ['host' => 'some.host']
                     ],
                 ],
-                false,
-                $baseResult
-            ],
-            [
-                $relationships,
-                [
-                    'db' => [
-                        'slave_connection' => [
-                            'default' => 'some connection data'
-                        ],
+                'resource' => [
+                    'default_setup' => [
+                        'connection' => 'default',
                     ],
                 ],
-                false,
-                $baseResult
-            ],
-            [
-                $relationships,
-                $ownConnection,
-                false,
-                array_replace_recursive($baseResult, $ownConnection),
-            ],
-            [
-                $relationships,
-                [],
-                true,
-                $resultWithSlave
-            ],
-            [
-                $relationships,
-                [
-                    'db' => [
-                        'connection' => [
-                            'default' => 'some connection data'
-                        ],
-                    ],
-                ],
-                true,
-                $resultWithSlave
-            ],
-            [
-                $relationships,
-                [
-                    'db' => [
-                        'slave_connection' => [
-                            'default' => 'some connection data'
-                        ],
-                    ],
-                ],
-                true,
-                $resultWithSlave
-            ],
-            [
-                $relationships,
-                $ownConnection,
-                true,
-                array_replace_recursive($resultWithSlave, $ownConnection),
-            ],
-            [
-                $relationshipsWithoutSlave,
-                [
-                    'db' => [
-                        'slave_connection' => [
-                            'default' => 'some connection data'
-                        ],
-                    ],
-                ],
-                true,
-                $baseResult,
-            ],
-        ];
+            ]);
+
+        $this->process->execute();
     }
 }
