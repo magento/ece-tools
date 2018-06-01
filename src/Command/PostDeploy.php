a9df8f9d499cc27dd6fd4dc7547c5f2f3adfc712
Ben Batschelet <bbatschelet@magento.com>
2018-06-01 10:13:23 -0500

    Move package info to build & deploy commands Bump build, deploy, & post-deploy logs to notice Drop routes down to debug

--- src/Command/PostDeploy.php
+++ src/Command/PostDeploy.php
@@ -70,13 +70,13 @@ class PostDeploy extends Command
     {
         try {
             if ($this->flagManager->exists(FlagManager::FLAG_DEPLOY_HOOK_IS_FAILED)) {
-                $this->logger->info('Post-deploy is skipped because deploy was failed.');
+                $this->logger->warning('Post-deploy is skipped because deploy was failed.');
                 return;
             }
 
-            $this->logger->info('Starting post-deploy.');
+            $this->logger->notice('Starting post-deploy.');
             $this->process->execute();
-            $this->logger->info('Post-deploy is complete.');
+            $this->logger->notice('Post-deploy is complete.');
         } catch (\Exception $exception) {
             $this->logger->critical($exception->getMessage());
 
