a34cb8d95664ed7c6889d4b274cabb56cfc9a963
Bohdan Korablov <bkorablov@magento.com>
2018-07-04 17:52:27 +0300

    MAGECLOUD-2406: Test creation of .magento.env.yaml.dist

---
+++ src/Test/Integration/MagentoEnvYamlTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\MagentoCloud\Test\Integration;
+
+use Magento\MagentoCloud\Config\Schema;
+use Magento\MagentoCloud\Filesystem\Driver\File;
+use Magento\MagentoCloud\App\ContainerInterface;
+use Magento\MagentoCloud\Config\StageConfigInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @inheritdoc
+ */
+class MagentoEnvYamlTest extends TestCase
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    private $skipVariables = [
+        StageConfigInterface::VAR_DEPLOY_FROM_GIT_OPTIONS,
+        StageConfigInterface::VAR_DEPLOYED_MAGENTO_VERSION_FROM_GIT,
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->container = Bootstrap::getInstance()
+            ->createApplication()
+            ->getContainer();
+    }
+
+    /**
+     * Check if the file dist/.magento.env.yaml contains a description of all variables
+     */
+    public function testCheckVariables()
+    {
+        /** @var Schema $schema */
+        $schema = $this->container->get(Schema::class);
+        /** @var File $file */
+        $file = $this->container->get(File::class);
+        $path = '/dist/.magento.env.yaml';
+        $content = $file->fileGetContents(ECE_BP . $path);
+        $forgottenVariables = [];
+
+        foreach (array_keys($schema->getSchema()) as $variable) {
+            if (in_array($variable, $this->skipVariables)) {
+                continue;
+            }
+
+            if (!preg_match('|# ' . $variable .'|', $content)) {
+                $forgottenVariables[] = $variable;
+            }
+        }
+
+        if ($forgottenVariables) {
+            $this->fail(
+                'You forgot to add the next variables into '
+                . $path . PHP_EOL . implode(', ', $forgottenVariables)
+            );
+        }
+    }
+}
