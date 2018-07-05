8cc06f7207659e1c7585b4b40d029d2a05f96090
Jacob Brown <jacob@gnu.org>
2018-07-05 15:08:53 -0500

    more travis debugging

--- tests/integration/AbstractTest.php
+++ tests/integration/AbstractTest.php
@@ -43,7 +43,9 @@ abstract class AbstractTest extends TestCase
      */
     public static function setUpBeforeClass()
     {
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         Bootstrap::getInstance()->run();
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
     }
 
     /**
@@ -51,7 +53,9 @@ abstract class AbstractTest extends TestCase
      */
     public static function tearDownAfterClass()
     {
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         Bootstrap::getInstance()->destroy();
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
     }
 
     /**
@@ -59,12 +63,17 @@ abstract class AbstractTest extends TestCase
      */
     protected function setUp()
     {
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->bootstrap = Bootstrap::getInstance();
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->container = Bootstrap::getInstance()->createApplication()->getContainer();
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->shell = $this->container->get(ShellInterface::class);
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->connection = $this->container->get(ConnectionInterface::class);
-
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->shell->execute('composer install -n --no-dev --no-progress');
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
     }
 
     /**
@@ -72,8 +81,12 @@ abstract class AbstractTest extends TestCase
      */
     protected function tearDown()
     {
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->shell->execute('php ./bin/magento setup:uninstall -n');
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->shell->execute('rm -rf vendor/*');
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
         $this->connection->close();
+        printf("Travis Debugging: %s:%d\n", __FILE__, __LINE__);
     }
 }
