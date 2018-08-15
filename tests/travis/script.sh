b53c3c0338ae9ad137a76fc665cb14d75c98acbb
Ben Batschelet <batschel@adobe.com>
2018-08-15 11:38:23 -0500

    Simplify Travis build & script

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -10,13 +10,10 @@ BASH="docker-compose run cli bash"
 
 case $TEST_SUITE in
     static-unit)
-        $BASH -c "${DIR_TOOLS}/vendor/bin/phpcs ${DIR_TOOLS}/src --standard=${DIR_TOOLS}/tests/static/phpcs-ruleset.xml -p -n"
-        $BASH -c "${DIR_TOOLS}/vendor/bin/phpmd ${DIR_TOOLS}/src xml ${DIR_TOOLS}/tests/static/phpmd-ruleset.xml"
-        $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --configuration ${DIR_TOOLS}/tests/unit --coverage-clover ${DIR_TOOLS}/tests/unit/tmp/clover.xml && php ${DIR_TOOLS}/tests/unit/code-coverage.php ${DIR_TOOLS}/tests/unit/tmp/clover.xml"
-        $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --configuration ${DIR_TOOLS}/tests/unit"
+        composer test
         ;;
     integration)
-        case $PHP in
+        case $TRAVIS_PHP_VERSION in
             7.0)
                 $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --group php70 --verbose --configuration ${DIR_TOOLS}/tests/integration"
                 ;;
