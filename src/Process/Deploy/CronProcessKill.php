07bbfbcc28fd5514566febdaa05e52ab97abda70
Oleh Posyniak <oposyniak@magento.com>
2018-09-12 13:09:16 -0500

    MAGECLOUD-2711: Cron kill during redeploy doesn't work

--- src/Process/Deploy/CronProcessKill.php
+++ src/Process/Deploy/CronProcessKill.php
@@ -46,7 +46,7 @@ class CronProcessKill implements ProcessInterface
         try {
             $this->logger->info('Trying to kill running cron jobs');
 
-            $cronPids = $this->shell->execute('exec pgrep -U "$USER" -f "bin/magento cron:run"');
+            $cronPids = $this->shell->execute('exec pgrep -U "$(id -u)" -f "bin/magento cron:run"');
             foreach ($cronPids as $pid) {
                 $this->killProcess($pid);
             }
