7f6863f01dc07c86b8cd23b796d450b064166b3d
Bohdan Korablov <bkorablov@magento.com>
2018-09-19 08:59:33 +0300

    MAGECLOUD-2729: Reading environment-specific .magento.env.yaml doesn't work on build phase

--- src/Test/Unit/Config/Environment/ReaderTest.php
+++ src/Test/Unit/Config/Environment/ReaderTest.php
@@ -5,11 +5,9 @@
  */
 namespace Magento\MagentoCloud\Test\Unit\Config\Environment;
 
-use Magento\MagentoCloud\Config\Environment;
 use Magento\MagentoCloud\Config\Environment\Reader;
 use Magento\MagentoCloud\Filesystem\ConfigFileList;
 use Magento\MagentoCloud\Filesystem\Driver\File;
-use Magento\MagentoCloud\Filesystem\SystemList;
 use phpmock\phpunit\PHPMock;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -20,16 +18,6 @@ use PHPUnit\Framework\TestCase;
 class ReaderTest extends TestCase
 {
     use PHPMock;
-
-    /**
-     * @var SystemList|MockObject
-     */
-    private $systemListMock;
-
-    /**
-     * @var Environment|MockObject
-     */
-    private $environmentMock;
 
     /**
      * @var ConfigFileList|MockObject
@@ -57,14 +45,10 @@ class ReaderTest extends TestCase
         self::defineFunctionMock('Magento\MagentoCloud\Filesystem\Driver', 'file_get_contents');
         self::defineFunctionMock('Magento\MagentoCloud\Filesystem\Driver', 'file_exists');
 
-        $this->systemListMock = $this->createMock(SystemList::class);
-        $this->environmentMock = $this->createMock(Environment::class);
         $this->configFileListMock = $this->createMock(ConfigFileList::class);
         $this->fileMock = $this->createPartialMock(File::class, []);
 
         $this->reader = new Reader(
-            $this->systemListMock,
-            $this->environmentMock,
             $this->configFileListMock,
             $this->fileMock
         );
@@ -77,98 +61,18 @@ class ReaderTest extends TestCase
         $this->configFileListMock->expects($this->once())
             ->method('getEnvConfig')
             ->willReturn($baseDir . '/.magento.env.yaml');
-        $this->systemListMock->expects($this->once())
-            ->method('getMagentoRoot')
-            ->willReturn($baseDir);
-        $this->environmentMock->expects($this->once())
-            ->method('getBranchName')
-            ->willReturn('test-branch');
 
         $this->reader->read();
         $this->assertEquals(
             [
                 'stage' => [
-                    'global' => ['SCD_ON_DEMAND' => false, 'UPDATE_URLS' => false],
-                    'deploy' => ['DATABASE_CONFIGURATION' => ['host' => 'localhost'], 'SCD_THREADS' => 3],
-                    'build' => ['SCD_THREADS' => 2],
-                ],
-                'log' => [
-                    'gelf' => [
-                        'min_level' => 'info',
-                        'use_default_formatter' => true,
-                        'additional' => ['project' => 'project'],
+                    'global' => [
+                        'SCD_ON_DEMAND' => true,
+                        'UPDATE_URLS' => false
                     ],
-                    'syslog' => ['ident' => 'ident-branch', 'facility' => 7],
-                ],
-            ],
-            $this->reader->read()
-        );
-    }
-
-    public function testReadBranchConfigNotExists()
-    {
-        $baseDir = __DIR__ . '/_file/';
-
-        $this->configFileListMock->expects($this->once())
-            ->method('getEnvConfig')
-            ->willReturn($baseDir . '/.magento.env.yaml');
-        $this->systemListMock->expects($this->once())
-            ->method('getMagentoRoot')
-            ->willReturn($baseDir);
-        $this->environmentMock->expects($this->once())
-            ->method('getBranchName')
-            ->willReturn('not-exist');
-
-        $this->assertEquals(
-            [
-                'stage' => [
-                    'global' => ['SCD_ON_DEMAND' => true, 'UPDATE_URLS' => false],
                     'deploy' => [
-                        'DATABASE_CONFIGURATION' => [
-                            'host' => '127.0.0.1',
-                            'port' => '3306',
-                            'schema' => 'test_schema',
-                        ],
-                        'SCD_THREADS' => 5,
-                    ],
-                ],
-                'log' => [
-                    'gelf' => [
-                        'min_level' => 'info',
-                        'use_default_formatter' => true,
-                        'additional' => ['project' => 'project', 'app_id' => 'app'],
-                    ],
-                ],
-            ],
-            $this->reader->read()
-        );
-    }
-
-    public function testReadBranchConfigWithEmptySectionAndStage()
-    {
-        $baseDir = __DIR__ . '/_file/';
-
-        $this->configFileListMock->expects($this->once())
-            ->method('getEnvConfig')
-            ->willReturn($baseDir . '/.magento.env.yaml');
-        $this->systemListMock->expects($this->once())
-            ->method('getMagentoRoot')
-            ->willReturn($baseDir);
-        $this->environmentMock->expects($this->once())
-            ->method('getBranchName')
-            ->willReturn('test-branch-emty');
-
-        $this->assertEquals(
-            [
-                'stage' => [
-                    'global' => ['SCD_ON_DEMAND' => true, 'UPDATE_URLS' => false],
-                    'deploy' => [
-                        'DATABASE_CONFIGURATION' => [
-                            'host' => '127.0.0.1',
-                            'port' => '3306',
-                            'schema' => 'test_schema',
-                        ],
-                        'SCD_THREADS' => 5,
+                        'DATABASE_CONFIGURATION' => ['host' => '127.0.0.1', 'port' => 3306, 'schema' => 'test_schema'],
+                        'SCD_THREADS' => 5
                     ],
                 ],
                 'log' => [
@@ -190,29 +94,16 @@ class ReaderTest extends TestCase
         $this->configFileListMock->expects($this->once())
             ->method('getEnvConfig')
             ->willReturn($baseDir . '/.magento-with-empty-sections.env.yaml');
-        $this->systemListMock->expects($this->once())
-            ->method('getMagentoRoot')
-            ->willReturn($baseDir);
-        $this->environmentMock->expects($this->once())
-            ->method('getBranchName')
-            ->willReturn('test-branch');
 
         $this->reader->read();
         $this->assertEquals(
             [
                 'stage' => [
-                    'global' => ['SCD_ON_DEMAND' => false],
-                    'deploy' => ['DATABASE_CONFIGURATION' => ['host' => 'localhost'], 'SCD_THREADS' => 3],
-                    'build' => ['SCD_THREADS' => 2],
+                    'global' => ['SCD_ON_DEMAND' => true],
+                    'deploy' => null,
+                    'build' => null,
                 ],
-                'log' => [
-                    'gelf' => [
-                        'min_level' => 'info',
-                        'use_default_formatter' => true,
-                        'additional' => ['project' => 'project'],
-                    ],
-                    'syslog' => ['ident' => 'ident-branch', 'facility' => 7],
-                ],
+                'log' => null,
             ],
             $this->reader->read()
         );
