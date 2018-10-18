04bfcceed143687c4c448d02412991b288592b61
Oleh Posyniak <oposyniak@magento.com>
2018-10-18 11:03:25 -0500

    MAGECLOUD-2577: Add possibility to import DB dump

--- src/Docker/DevBuilder.php
+++ src/Docker/DevBuilder.php
@@ -106,6 +106,7 @@ class DevBuilder implements BuilderInterface
                 'appdata' => [
                     'image' => 'tianon/true',
                     'volumes' => [
+                        './docker/mnt:/mnt',
                         '/var/www/magento/vendor',
                         '/var/www/magento/generated',
                         '/var/www/magento/pub',
