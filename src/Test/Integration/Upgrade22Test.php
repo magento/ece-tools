19b4549b60fa6ab77f1a6d6c4ec349f890e91b80
Oleh Posyniak <oposyniak@magento.com>
2018-12-12 14:18:49 +0200

    MAGECLOUD-2907: [Cloud Docker] Verify sendmail functionality

--- src/Test/Integration/Upgrade22Test.php
+++ src/Test/Integration/Upgrade22Test.php
@@ -26,47 +26,6 @@ class Upgrade22Test extends AbstractTest
     }
 
     /**
-     * @param string $fromVersion
-     * @param string $toVersion
-     * @dataProvider defaultDataProvider
-     */
-    public function testDefault(string $fromVersion, string $toVersion)
-    {
-        $this->bootstrap->execute(sprintf(
-            'rm -rf %s/vendor/*',
-            $this->bootstrap->getSandboxDir()
-        ));
-
-        $application = $this->bootstrap->createApplication([]);
-
-        $executeAndAssert = function ($commandName) use ($application) {
-            $application->getContainer()->set(
-                \Psr\Log\LoggerInterface::class,
-                \Magento\MagentoCloud\App\Logger::class
-            );
-
-            $commandTester = new CommandTester($application->get($commandName));
-            $commandTester->execute([]);
-            $this->assertSame(0, $commandTester->getStatusCode());
-        };
-
-        $this->updateToVersion($fromVersion);
-
-        $executeAndAssert(Build::NAME);
-        $executeAndAssert(Deploy::NAME);
-        $executeAndAssert(PostDeploy::NAME);
-
-        $this->assertContentPresence();
-        $this->updateToVersion($toVersion);
-
-        $executeAndAssert(Build::NAME);
-        $executeAndAssert(Deploy::NAME);
-        $executeAndAssert(PostDeploy::NAME);
-
-        $this->assertContentPresence();
-    }
-
-    /**
      * @return array
      */
     public function defaultDataProvider(): array
@@ -74,32 +33,5 @@ class Upgrade22Test extends AbstractTest
         return [
             ['2.2.0', '2.2.*'],
         ];
-    }
-
-    private function assertContentPresence()
-    {
-        $routes = $this->bootstrap->getEnv('routes', []);
-        $defaultRoute = array_keys($routes)[0];
-        $pageContent = file_get_contents($defaultRoute);
-
-        $this->assertContains('Home Page', $pageContent, 'Check "Home Page" phrase presence');
-    }
-
-    /**
-     * @param string $version
-     */
-    private function updateToVersion($version)
-    {
-        $sandboxDir = $this->bootstrap->getSandboxDir();
-        $this->bootstrap->execute(sprintf(
-            'rm -rf %s/vendor/*',
-            $sandboxDir
-        ));
-        $this->bootstrap->execute(sprintf(
-            'composer require magento/product-enterprise-edition %s --no-update -n -d %s',
-            $version,
-            $sandboxDir
-        ));
-        $this->bootstrap->execute(sprintf('composer update -n -d %s', $sandboxDir));
     }
 }
