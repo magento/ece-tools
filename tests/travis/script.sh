0449f4cc1a09da22f14ea626e7d08211cbf31379
Oleh Posyniak <oposyniak@magento.com>
2019-01-30 12:39:20 -0600

    MAGECLOUD-3008: Integration Testing on Magento Cloud Docker

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -10,12 +10,18 @@ BASH="docker-compose run cli bash"
 
 case $TEST_SUITE in
     static-unit)
+        docker-compose up -d
+
         $BASH -c "${DIR_TOOLS}/vendor/bin/phpcs ${DIR_TOOLS}/src --standard=${DIR_TOOLS}/tests/static/phpcs-ruleset.xml -p -n"
         $BASH -c "${DIR_TOOLS}/vendor/bin/phpmd ${DIR_TOOLS}/src xml ${DIR_TOOLS}/tests/static/phpmd-ruleset.xml"
         $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --configuration ${DIR_TOOLS}/tests/unit --coverage-clover ${DIR_TOOLS}/tests/unit/tmp/clover.xml && php ${DIR_TOOLS}/tests/unit/code-coverage.php ${DIR_TOOLS}/tests/unit/tmp/clover.xml"
         $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --configuration ${DIR_TOOLS}/tests/unit"
+
+        docker-compose down -v
         ;;
     integration)
+        docker-compose up -d
+
         case $TRAVIS_PHP_VERSION in
             7.0)
                 $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --group php70 --verbose --configuration ${DIR_TOOLS}/tests/integration"
@@ -27,5 +33,10 @@ case $TEST_SUITE in
                 $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --group php72 --verbose --configuration ${DIR_TOOLS}/tests/integration"
                 ;;
         esac
+
+        docker-compose down -v
+        ;;
+    docker-integration)
+        $BASH -c "${DIR_TOOLS}/vendor/bin/phpunit --verbose --configuration ${DIR_TOOLS}/tests/docker-integration"
         ;;
 esac
