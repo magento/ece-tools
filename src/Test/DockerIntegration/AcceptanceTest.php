3cc9a65434265a973e2a39be23ff2cff5601e712
Oleh Posyniak <oposyniak@magento.com>
2019-03-14 11:52:45 -0500

    MAGECLOUD-3275: Rewrite ConfigDumpTest to use Cloud Docker

--- src/Test/DockerIntegration/AcceptanceTest.php
+++ src/Test/DockerIntegration/AcceptanceTest.php
@@ -6,11 +6,17 @@
 namespace Magento\MagentoCloud\Test\DockerIntegration;
 
 use Magento\MagentoCloud\Test\DockerIntegration\Process;
+use Magento\MagentoCloud\Util\ArrayManager;
 
 /**
  * @inheritdoc
  *
  * @php 7.2
+ *
+ * 1. Test successful deploy
+ * 2. Test content presence
+ * 3. Test config dump
+ * 4. Test content presence
  */
 class AcceptanceTest extends AbstractTest
 {
@@ -41,11 +47,44 @@ class AcceptanceTest extends AbstractTest
 
         $this->assertSame(0, $code);
 
-        $process = new Process\Curl();
-        $process->setTimeout(null)
+        $assertContentPresence = function () {
+            $process = new Process\Curl();
+            $process->setTimeout(null)
+                ->run();
+
+            $this->assertSame(0, $process->getExitCode());
+            $this->assertContains('Home page', $process->getOutput());
+        };
+
+        $assertContentPresence();
+
+        $code = (new Process\Ece('config:dump', Config::CONTAINER_DEPLOY))
             ->run();
 
-        $this->assertSame(0, $process->getExitCode());
-        $this->assertContains('Home page', $process->getOutput());
+        $this->assertSame(0, $code);
+
+        $to = sys_get_temp_dir() . '/app/etc/config.php';
+
+        $read = new Process\Copy('/app/etc/config.php', $to);
+        $read->mustRun();
+
+        $config = require $to;
+
+        $arrayManager = new ArrayManager();
+        $flattenKeysConfig = implode(array_keys($arrayManager->flatten($config, '#')));
+
+        $this->assertContains('#modules', $flattenKeysConfig);
+        $this->assertContains('#scopes', $flattenKeysConfig);
+        $this->assertContains('#system/default/general/locale/code', $flattenKeysConfig);
+        $this->assertContains('#system/default/dev/static/sign', $flattenKeysConfig);
+        $this->assertContains('#system/default/dev/front_end_development_workflow', $flattenKeysConfig);
+        $this->assertContains('#system/default/dev/template', $flattenKeysConfig);
+        $this->assertContains('#system/default/dev/js', $flattenKeysConfig);
+        $this->assertContains('#system/default/dev/css', $flattenKeysConfig);
+        $this->assertContains('#system/stores', $flattenKeysConfig);
+        $this->assertContains('#system/websites', $flattenKeysConfig);
+        $this->assertContains('#admin_user/locale/code', $flattenKeysConfig);
+
+        $assertContentPresence();
     }
 }
