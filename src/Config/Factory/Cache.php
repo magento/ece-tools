a52a8c3286af8d1cc38bbba0e907ab705be6d55a
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2019-03-27 13:02:51 -0500

    MAGECLOUD-2899: Redis Slave Nodes Have no Health Checks / Are not Stable

--- src/Config/Factory/Cache.php
+++ src/Config/Factory/Cache.php
@@ -100,10 +100,10 @@ class Cache
         $slaveConnectionData = $this->getSlaveConnection();
         if ($slaveConnectionData) {
             if ($this->isConfigurationCompatibleWithSlaveConnection($envCacheConfiguration, $redisConfig)) {
-                $redisCache['frontend_options']['write_control'] = false;
                 $redisCache['backend_options']['load_from_slave'] = $slaveConnectionData;
-                $redisCache['backend_options']['retry_reads_on_master'] = '1';
                 $redisCache['backend_options']['read_timeout'] = '1';
+                $redisCache['backend_options']['retry_reads_on_master'] = '1';
+                $redisCache['frontend_options']['write_control'] = false;
                 $this->logger->info('Set Redis slave connection');
             } else {
                 $this->logger->notice(
