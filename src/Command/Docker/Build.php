5ec4920f438dd0a0852ada52968524f89d7f32f6
Ben Batschelet <batschel@adobe.com>
2019-04-22 15:01:46 -0500

    Value is required for node option. Config convert command does not use the same input as build, so do not forward it

--- src/Command/Docker/Build.php
+++ src/Command/Docker/Build.php
@@ -16,6 +16,7 @@ use Magento\MagentoCloud\Docker\ConfigurationMismatchException;
 use Magento\MagentoCloud\Filesystem\Driver\File;
 use Magento\MagentoCloud\Filesystem\FileSystemException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -125,7 +126,7 @@ class Build extends Command
             )->addOption(
                 self::OPTION_NODE,
                 null,
-                InputOption::VALUE_OPTIONAL,
+                InputOption::VALUE_REQUIRED,
                 'Node.js version'
             );
 
@@ -168,7 +169,7 @@ class Build extends Command
 
         $this->getApplication()
             ->find(ConfigConvert::NAME)
-            ->run($input, $output);
+            ->run(new ArrayInput([]), $output);
 
         $output->writeln('<info>Configuration was built.</info>');
     }
