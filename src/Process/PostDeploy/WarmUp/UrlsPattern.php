87b51506f3ae8e5302007d3d1f01eadddb753e8c
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-06-13 10:30:46 -0500

    MAGECLOUD-3258: Cache Warm-up, Using RegEx to Warm-up Multiple Pages

--- src/Process/PostDeploy/WarmUp/UrlsPattern.php
+++ src/Process/PostDeploy/WarmUp/UrlsPattern.php
@@ -64,7 +64,7 @@ class UrlsPattern
 
             $process = $this->shell->execute($command);
 
-            $urls = json_decode($process->getOutput());
+            $urls = json_decode($process->getOutput(), true);
 
             if (json_last_error() !== JSON_ERROR_NONE) {
                 $this->logger->error(sprintf(
