8351bdebb415a6c5a7867d7f2da6ce78a66c4a27
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-07-22 11:36:48 -0500

    MAGECLOUD-3578: Add PHPStan to Static Tests set

--- tests/travis/static-unit.sh
+++ tests/travis/static-unit.sh
@@ -7,7 +7,7 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 if [ $TRAVIS_PHP_VERSION = "7.0" ]; then
-   ./vendor/bin/phpstan analyse -c ./tests/static/phpstan7.0.neon
+   ./vendor/bin/phpstan analyse -l 1 -c ./tests/static/phpstan7.0.neon ./src
 else
    ./vendor/bin/phpstan analyse -c ./tests/static/phpstan.neon
 fi
