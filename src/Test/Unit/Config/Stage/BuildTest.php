13bee7acc5812828ba416192a094c772f955b0a2
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-09-11 09:39:57 -0500

    MAGECLOUD-3874: Remove support if build_options.ini and fail build if it's present (#590)

--- src/Test/Unit/Config/Stage/BuildTest.php
+++ src/Test/Unit/Config/Stage/BuildTest.php
@@ -11,7 +11,6 @@ use Magento\MagentoCloud\Config\Stage\BuildInterface;
 use Magento\MagentoCloud\Config\StageConfigInterface;
 use PHPUnit\Framework\TestCase;
 use Magento\MagentoCloud\Config\Environment\Reader as EnvironmentReader;
-use Magento\MagentoCloud\Config\Build\Reader as BuildReader;
 use PHPUnit_Framework_MockObject_MockObject as Mock;
 
 /**
@@ -30,11 +29,6 @@ class BuildTest extends TestCase
     private $environmentReaderMock;
 
     /**
-     * @var BuildReader|Mock
-     */
-    private $buildReaderMock;
-
-    /**
      * @var Schema|Mock
      */
     private $schemaMock;
@@ -45,7 +39,6 @@ class BuildTest extends TestCase
     protected function setUp()
     {
         $this->environmentReaderMock = $this->createMock(EnvironmentReader::class);
-        $this->buildReaderMock = $this->createMock(BuildReader::class);
         $this->schemaMock = $this->createMock(Schema::class);
         $this->schemaMock->expects($this->any())
             ->method('getDefaults')
@@ -62,7 +55,6 @@ class BuildTest extends TestCase
 
         $this->config = new Build(
             $this->environmentReaderMock,
-            $this->buildReaderMock,
             $this->schemaMock
         );
     }
@@ -70,18 +62,14 @@ class BuildTest extends TestCase
     /**
      * @param string $name
      * @param array $envConfig
-     * @param array $buildConfig
      * @param mixed $expectedValue
      * @dataProvider getDataProvider
      */
-    public function testGet(string $name, array $envConfig, array $buildConfig, $expectedValue)
+    public function testGet(string $name, array $envConfig, $expectedValue)
     {
         $this->environmentReaderMock->expects($this->once())
             ->method('read')
             ->willReturn([Build::SECTION_STAGE => $envConfig]);
-        $this->buildReaderMock->expects($this->once())
-            ->method('read')
-            ->willReturn($buildConfig);
 
         $this->assertSame($expectedValue, $this->config->get($name));
     }
@@ -96,7 +84,6 @@ class BuildTest extends TestCase
             'default strategy' => [
                 Build::VAR_SCD_STRATEGY,
                 [],
-                [],
                 '',
             ],
             'env configured strategy' => [
@@ -107,7 +94,6 @@ class BuildTest extends TestCase
                         Build::VAR_SCD_STRATEGY => 'simple',
                     ],
                 ],
-                [],
                 'simple',
             ],
             'global env strategy' => [
@@ -118,7 +104,6 @@ class BuildTest extends TestCase
                     ],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 'simple',
             ],
             'default strategy with parameter' => [
@@ -127,25 +112,10 @@ class BuildTest extends TestCase
                     StageConfigInterface::STAGE_GLOBAL => [],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 '',
-            ],
-            'build configured strategy' => [
-                Build::VAR_SCD_STRATEGY,
-                [
-                    StageConfigInterface::STAGE_GLOBAL => [],
-                    StageConfigInterface::STAGE_BUILD => [
-                        Build::VAR_SCD_STRATEGY => 'simple',
-                    ],
-                ],
-                [
-                    'scd_strategy' => 'quick',
-                ],
-                'quick',
             ],
             'default exclude_themes' => [
                 Build::VAR_SCD_EXCLUDE_THEMES,
-                [],
                 [],
                 '',
             ],
@@ -157,7 +127,6 @@ class BuildTest extends TestCase
                         Build::VAR_SCD_EXCLUDE_THEMES => 'luma',
                     ],
                 ],
-                [],
                 'luma',
             ],
             'global env exclude_themes' => [
@@ -168,7 +137,6 @@ class BuildTest extends TestCase
                     ],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 'luma',
             ],
             'default exclude_themes with parameter' => [
@@ -177,25 +145,10 @@ class BuildTest extends TestCase
                     StageConfigInterface::STAGE_GLOBAL => [],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 '',
-            ],
-            'build configured exclude_themes' => [
-                Build::VAR_SCD_EXCLUDE_THEMES,
-                [
-                    StageConfigInterface::STAGE_GLOBAL => [],
-                    StageConfigInterface::STAGE_BUILD => [
-                        Build::VAR_SCD_EXCLUDE_THEMES => 'luma',
-                    ],
-                ],
-                [
-                    'exclude_themes' => 'blank',
-                ],
-                'blank',
             ],
             'default compress level' => [
                 Build::VAR_SCD_COMPRESSION_LEVEL,
-                [],
                 [],
                 6,
             ],
@@ -207,7 +160,6 @@ class BuildTest extends TestCase
                         Build::VAR_SCD_COMPRESSION_LEVEL => 5,
                     ],
                 ],
-                [],
                 5,
             ],
             'global env compress level' => [
@@ -218,7 +170,6 @@ class BuildTest extends TestCase
                     ],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 5,
             ],
             'default compress level with parameter' => [
@@ -227,25 +178,10 @@ class BuildTest extends TestCase
                     StageConfigInterface::STAGE_GLOBAL => [],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 6,
-            ],
-            'build configured compress level' => [
-                Build::VAR_SCD_COMPRESSION_LEVEL,
-                [
-                    StageConfigInterface::STAGE_GLOBAL => [],
-                    StageConfigInterface::STAGE_BUILD => [
-                        Build::VAR_SCD_COMPRESSION_LEVEL => 5,
-                    ],
-                ],
-                [
-                    'SCD_COMPRESSION_LEVEL' => 7,
-                ],
-                7,
             ],
             'default scd_threads' => [
                 Build::VAR_SCD_THREADS,
-                [],
                 [],
                 1,
             ],
@@ -257,7 +193,6 @@ class BuildTest extends TestCase
                         Build::VAR_SCD_THREADS => 5,
                     ],
                 ],
-                [],
                 5,
             ],
             'global env scd_threads' => [
@@ -268,7 +203,6 @@ class BuildTest extends TestCase
                     ],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 5,
             ],
             'default scd_threads with parameter' => [
@@ -277,25 +211,10 @@ class BuildTest extends TestCase
                     StageConfigInterface::STAGE_GLOBAL => [],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 1,
-            ],
-            'build configured scd_threads' => [
-                Build::VAR_SCD_THREADS,
-                [
-                    StageConfigInterface::STAGE_GLOBAL => [],
-                    StageConfigInterface::STAGE_BUILD => [
-                        Build::VAR_SCD_THREADS => 5,
-                    ],
-                ],
-                [
-                    'scd_threads' => 7,
-                ],
-                7,
             ],
             'default skip_scd' => [
                 Build::VAR_SKIP_SCD,
-                [],
                 [],
                 false,
             ],
@@ -307,7 +226,6 @@ class BuildTest extends TestCase
                         Build::VAR_SKIP_SCD => true,
                     ],
                 ],
-                [],
                 true,
             ],
             'global env skip_scd' => [
@@ -318,7 +236,6 @@ class BuildTest extends TestCase
                     ],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 true,
             ],
             'default skip_scd with parameter' => [
@@ -327,25 +244,10 @@ class BuildTest extends TestCase
                     StageConfigInterface::STAGE_GLOBAL => [],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 false,
-            ],
-            'build configured skip_scd' => [
-                Build::VAR_SKIP_SCD,
-                [
-                    StageConfigInterface::STAGE_GLOBAL => [],
-                    StageConfigInterface::STAGE_BUILD => [
-                        Build::VAR_SKIP_SCD => false,
-                    ],
-                ],
-                [
-                    'skip_scd' => '1',
-                ],
-                true,
             ],
             'default verbose commands' => [
                 Build::VAR_VERBOSE_COMMANDS,
-                [],
                 [],
                 '',
             ],
@@ -357,7 +259,6 @@ class BuildTest extends TestCase
                         Build::VAR_VERBOSE_COMMANDS => '-v',
                     ],
                 ],
-                [],
                 '-v',
             ],
             'global env verbose commands' => [
@@ -368,7 +269,6 @@ class BuildTest extends TestCase
                     ],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 '-v',
             ],
             'default verbose commands with parameter' => [
@@ -377,25 +277,10 @@ class BuildTest extends TestCase
                     StageConfigInterface::STAGE_GLOBAL => [],
                     StageConfigInterface::STAGE_BUILD => [],
                 ],
-                [],
                 '',
-            ],
-            'build configured verbose commands' => [
-                Build::VAR_VERBOSE_COMMANDS,
-                [
-                    StageConfigInterface::STAGE_GLOBAL => [],
-                    StageConfigInterface::STAGE_BUILD => [
-                        Build::VAR_VERBOSE_COMMANDS => '-vvv',
-                    ],
-                ],
-                [
-                    'VERBOSE_COMMANDS' => 'enabled',
-                ],
-                '-vv',
             ],
             'scd strategy default' => [
                 Build::VAR_SCD_STRATEGY,
-                [],
                 [],
                 ''
             ]
