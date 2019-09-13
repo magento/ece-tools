d9e9bc92275719ae66dbe0e27526592b987536ec
Bohdan Korablov <korablov@adobe.com>
2019-09-13 10:27:51 -0500

    MAGECLOUD-4242: Refactor ece-tools functional tests

--- src/Test/Functional/Acceptance/AbstractCest.php
+++ src/Test/Functional/Acceptance/AbstractCest.php
@@ -17,7 +17,7 @@ abstract class AbstractCest
      */
     public function _before(\CliTester $I)
     {
-//        $I->generateDockerCompose();
+        $I->generateDockerCompose();
         $I->cleanUpEnvironment();
     }
 
@@ -26,7 +26,7 @@ abstract class AbstractCest
      */
     public function _after(\CliTester $I)
     {
-//        $I->stopEnvironment();
-//        $I->removeDockerCompose();
+        $I->stopEnvironment();
+        $I->removeDockerCompose();
     }
 }
