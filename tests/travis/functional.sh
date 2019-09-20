8d6cfd9502409bf806062b1b2010db8099cd4a2d
Ben Batschelet <batschel@adobe.com>
2019-09-20 13:29:33 -0500

    Skip UpgradeCept for PHP 7.3 (there's nothing to upgrade to)

--- tests/travis/functional.sh
+++ tests/travis/functional.sh
@@ -10,7 +10,10 @@ case $TRAVIS_PHP_VERSION in
     7.1)
         ./vendor/bin/codecept run -g php71 --steps
         ;;
-    7.2 | 7.3)
+    7.2)
         ./vendor/bin/codecept run -g parallel_"$FUNCTIONAL_INDEX" --steps
+        ;;
+    7.3)
+        ./vendor/bin/codecept run -g parallel_"$FUNCTIONAL_INDEX" -x php72 --steps
         ;;
 esac
