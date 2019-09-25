2564585641655212483fb9885d280808de3539a2
sikharam-adb <54289725+sikharam-adb@users.noreply.github.com>
2019-09-25 12:54:50 -0500

    MAGECLOUD-4116: Adding a strict_types sniffer for ece-tools. (#610)

--- src/Test/Unit/Process/Deploy/PreDeploy/CleanRedisCacheTest.php
+++ src/Test/Unit/Process/Deploy/PreDeploy/CleanRedisCacheTest.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+declare(strict_types=1);
+
 namespace Magento\MagentoCloud\Test\Unit\Process\Deploy\PreDeploy;
 
 use Magento\MagentoCloud\Config\Environment;
