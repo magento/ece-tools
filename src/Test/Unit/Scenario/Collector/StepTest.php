8e92dd890ded510363417dd2162fc64ce9fb0194
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-10-10 09:37:14 -0500

    MAGECLOUD-4109: Introduce extensibility mechanism based on Dependency Injection

--- src/Test/Unit/Scenario/Collector/StepTest.php
+++ src/Test/Unit/Scenario/Collector/StepTest.php
@@ -154,6 +154,7 @@ class StepTest extends TestCase
     }
 
     /**
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      * @throws \Magento\MagentoCloud\Scenario\Exception\ValidationException
      */
     public function testCollect()
