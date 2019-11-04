f2de682243592381d09bd0722ab7acc53d2f713d
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-11-04 13:30:46 -0600

    MAGECLOUD-4454: Add one more index to Travis CI FT

--- tests/travis/prepare_functional_parallel.sh
+++ tests/travis/prepare_functional_parallel.sh
@@ -6,31 +6,27 @@
 set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
-test_set_list=$(grep -RL 'php71' src/Test/Functional/Acceptance | sort)
-test_set_count=$(printf "$test_set_list" | wc -l)
-let "test_set_count++"
+readarray -t test_set_list <<< "$(grep -RL 'php71' src/Test/Functional/Acceptance | sort -r)"
+group_count=4
 
-test_set_size[1]=$(printf "%.0f" $(echo "$test_set_count*0.20" | bc))
-test_set_size[2]=$(printf "%.0f" $(echo "$test_set_count*0.40" | bc))
-test_set_size[3]=$((test_set_count-test_set_size[1]-test_set_size[2]))
-echo "Total = ${test_set_count}; Batch #1 = ${test_set_size[1]}; Batch #2 = ${test_set_size[2]}; Batch #3 = ${test_set_size[3]};";
+element_in_group=$(printf "%.0f" "$(echo "scale=2;(${#test_set_list[@]} + ${group_count} - 1)/${group_count}" | bc)")
+
+echo "Total = ${#test_set_list[@]};"
 
 cp codeception.dist.yml codeception.yml
 echo "groups:" >> codeception.yml
 echo "  parallel_*: tests/functional/_data/parallel_*" >> codeception.yml
 
-group_id=1
-i=1
-for test_file in $test_set_list
+for((i=0, group_id=1; i < ${#test_set_list[@]}; i+=element_in_group, group_id++))
 do
-    group_file="tests/functional/_data/parallel_$group_id.yml"
-    echo "$test_file" >> "$group_file"
+  part=( "${test_set_list[@]:i:element_in_group}" )
+  echo "Batch #${group_id} = ${#part[@]}"
 
-    if [ $i -lt ${test_set_size[$group_id]} ]
-    then
-        let "i++"
-    else
-        i=1
-        let "group_id++"
-    fi
+  group_file="tests/functional/_data/parallel_$group_id.yml"
+
+  for test_file in "${part[@]}"
+  do
+    echo "$test_file" >> "$group_file"
+  done
 done
+exit;
