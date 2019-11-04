c88108ed9a27f211b91ca8a45d19e3f3399049b0
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-11-04 13:32:56 -0600

    MAGECLOUD-4454: Add one more index to Travis CI FT

--- tests/travis/prepare_functional_parallel.sh
+++ tests/travis/prepare_functional_parallel.sh
@@ -8,23 +8,22 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 
 readarray -t test_set_list <<< "$(grep -RL 'php71' src/Test/Functional/Acceptance | sort -r)"
 group_count=4
-
 element_in_group=$(printf "%.0f" "$(echo "scale=2;(${#test_set_list[@]} + ${group_count} - 1)/${group_count}" | bc)")
-
-echo "Total = ${#test_set_list[@]};"
 
 cp codeception.dist.yml codeception.yml
 echo "groups:" >> codeception.yml
 echo "  parallel_*: tests/functional/_data/parallel_*" >> codeception.yml
 
+echo "Total = ${#test_set_list[@]};"
+
 for((i=0, group_id=1; i < ${#test_set_list[@]}; i+=element_in_group, group_id++))
 do
-  part=( "${test_set_list[@]:i:element_in_group}" )
-  echo "Batch #${group_id} = ${#part[@]}"
+  test_file_group=( "${test_set_list[@]:i:element_in_group}" )
+  echo "Batch #${group_id} = ${#test_file_group[@]}"
 
   group_file="tests/functional/_data/parallel_$group_id.yml"
 
-  for test_file in "${part[@]}"
+  for test_file in "${test_file_group[@]}"
   do
     echo "$test_file" >> "$group_file"
   done
