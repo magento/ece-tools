3d0665f2b6cb76d130bbd1944531aa7f0d75256e
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2019-12-03 14:23:25 -0600

    MAGECLOUD-3905: Implement split DB on Cloud using ece-tools

--- src/Config/Database/MergedConfig.php
+++ src/Config/Database/MergedConfig.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 namespace Magento\MagentoCloud\Config\Database;
 
 use Magento\MagentoCloud\Config\ConfigMerger;
-use Magento\MagentoCloud\Config\Deploy\Reader as ConfigReader;
 use Magento\MagentoCloud\Config\Stage\DeployInterface;
 use Magento\MagentoCloud\DB\Data\ConnectionInterface;
 use Magento\MagentoCloud\DB\Data\RelationshipConnectionFactory;
@@ -18,19 +17,69 @@ use Magento\MagentoCloud\DB\Data\RelationshipConnectionFactory;
  */
 class MergedConfig implements ConfigInterface
 {
+    const RESOURCE = 'resource';
+
+    /**
+     * Names fo resources
+     */
+    const RESOURCE_CHECKOUT = 'checkout';
+    const RESOURCE_SALES = 'sales';
+    const RESOURCE_DEFAULT_SETUP = 'default_setup';
+
+    const DB = 'db';
+    const CONNECTION = 'connection';
+
+    /**
+     * Names of connections
+     */
+    const SLAVE_CONNECTION = 'slave_connection';
+    const CONNECTION_DEFAULT = 'default';
+    const CONNECTION_INDEXER = 'indexer';
+    const CONNECTION_CHECKOUT = 'checkout';
+    const CONNECTION_SALES = 'sales';
+
+    /**
+     * Types of connections
+     */
+    const CONNECTION_TYPES = [self::CONNECTION, self::SLAVE_CONNECTION];
+
+    /**
+     * Split connections
+     */
+    const SPLIT_CONNECTIONS = [self::CONNECTION_CHECKOUT, self::CONNECTION_SALES];
+
+    /**
+     * Connections map
+     */
+    const CONNECTION_MAP = [
+        self::CONNECTION => [
+            self::CONNECTION_DEFAULT => RelationshipConnectionFactory::CONNECTION_MAIN,
+            self::CONNECTION_INDEXER => RelationshipConnectionFactory::CONNECTION_MAIN,
+            self::CONNECTION_CHECKOUT => RelationshipConnectionFactory::CONNECTION_QUOTE_MAIN,
+            self::CONNECTION_SALES => RelationshipConnectionFactory::CONNECTION_SALES_MAIN,
+        ],
+        self::SLAVE_CONNECTION => [
+            self::CONNECTION_DEFAULT => RelationshipConnectionFactory::CONNECTION_SLAVE,
+            self::CONNECTION_CHECKOUT => RelationshipConnectionFactory::CONNECTION_QUOTE_SLAVE,
+            self::CONNECTION_SALES => RelationshipConnectionFactory::CONNECTION_SALES_SLAVE,
+        ]
+    ];
+
+    /**
+     * Resources map
+     */
+    const RESOURCE_MAP = [
+        self::CONNECTION_DEFAULT => self::RESOURCE_DEFAULT_SETUP,
+        self::CONNECTION_CHECKOUT => self::RESOURCE_CHECKOUT,
+        self::CONNECTION_SALES => self::RESOURCE_SALES,
+    ];
+
     /**
      * Final configuration for deploy phase
      *
      * @var DeployInterface
      */
     private $stageConfig;
-
-    /**
-     * Configuration for slave connection
-     *
-     * @var SlaveConfig
-     */
-    private $slaveConfig;
 
     /**
      * Class for configuration merging
@@ -42,16 +91,16 @@ class MergedConfig implements ConfigInterface
     /**
      * Connection data from relationship array
      *
-     * @var ConnectionInterface
+     * @var array
      */
     private $connectionData;
 
     /**
-     * Reader for app/etc/env.php file
+     * Factory for creation database configurations
      *
-     * @var ConfigReader
+     * @var RelationshipConnectionFactory
      */
-    private $configReader;
+    private $connectionDataFactory;
 
     /**
      * Final database configuration after merging
@@ -61,35 +110,32 @@ class MergedConfig implements ConfigInterface
     private $mergedConfig;
 
     /**
-     * Factory for creation database configurations
-     *
-     * @var RelationshipConnectionFactory
-     */
-    private $connectionFactory;
-
-    /**
-     * @param RelationshipConnectionFactory $connectionFactory
-     * @param ConfigReader $configReader
-     * @param SlaveConfig $slaveConfig
-     * @param DeployInterface $stageConfig
      * @param ConfigMerger $configMerger
+     * @param DeployInterface $stageConfig
+     * @param RelationshipConnectionFactory $connectionDataFactory
      */
     public function __construct(
-        RelationshipConnectionFactory $connectionFactory,
-        ConfigReader $configReader,
-        SlaveConfig $slaveConfig,
+        ConfigMerger $configMerger,
         DeployInterface $stageConfig,
-        ConfigMerger $configMerger
-    ) {
-        $this->connectionFactory = $connectionFactory;
-        $this->configReader = $configReader;
-        $this->slaveConfig = $slaveConfig;
+        RelationshipConnectionFactory $connectionDataFactory
+    )
+    {
+        $this->connectionDataFactory = $connectionDataFactory;
         $this->stageConfig = $stageConfig;
         $this->configMerger = $configMerger;
     }
 
     /**
-     * Returns database configuration.
+     * Returns database and resource configurations
+     *
+     * Returns
+     * ```
+     * [
+     *     'db' => [...]       // Database configuration
+     *     'resource' => [...] // Resource configuration
+     * ]
+     *
+     * ```
      *
      * @return array
      */
@@ -99,53 +145,82 @@ class MergedConfig implements ConfigInterface
             return $this->mergedConfig;
         }
 
-        $envDbConfig = $this->stageConfig->get(DeployInterface::VAR_DATABASE_CONFIGURATION);
-
-        if (!$this->configMerger->isEmpty($envDbConfig) && !$this->configMerger->isMergeRequired($envDbConfig)) {
-            return $this->mergedConfig = $this->configMerger->clear($envDbConfig);
-        }
-
-        if (!empty($this->getConnectionData()->getHost())) {
-            $dbConfig = $this->generateDbConfig();
-        } else {
-            $dbConfig = $this->getDbConfigFromEnvFile();
-        }
-
-        return $this->mergedConfig = $this->configMerger->merge($dbConfig, $envDbConfig);
+        $connections = $this->getConnections();
+        return $this->mergedConfig = [
+            self::DB => $connections,
+            self::RESOURCE => $this->getResources(array_keys($connections[self::CONNECTION])),
+        ];
     }
 
-    /**
-     * Generates database configuration from environment relationships.
-     *
-     * @return array
-     */
-    private function generateDbConfig(): array
+
+    private function getConnections(): array
     {
-        $connectionData = [
-            'username' => $this->getConnectionData()->getUser(),
-            'host' => $this->getConnectionData()->getHost(),
-            'dbname' => $this->getConnectionData()->getDbName(),
-            'password' => $this->getConnectionData()->getPassword(),
-        ];
+        $envConfig = $this->stageConfig->get(DeployInterface::VAR_DATABASE_CONFIGURATION);
 
-        $dbConfig = [
-            'connection' => [
-                'default' => $connectionData,
-                'indexer' => $connectionData,
-            ],
-        ];
-
-        if ($this->stageConfig->get(DeployInterface::VAR_MYSQL_USE_SLAVE_CONNECTION)
-            && $this->isDbConfigurationCompatibleWithSlaveConnection()
-        ) {
-            $slaveConfiguration = $this->slaveConfig->get();
-
-            if (!empty($slaveConfiguration)) {
-                $dbConfig['slave_connection']['default'] = $slaveConfiguration;
+        foreach (self::SPLIT_CONNECTIONS as $connectionName) {
+            foreach (self::CONNECTION_TYPES as $connectionType) {
+                if (isset($envConfig[$connectionType][$connectionName])) {
+                    unset($envConfig[$connectionType][$connectionName]);
+                }
             }
         }
 
-        return $dbConfig;
+        if (!$this->configMerger->isEmpty($envConfig) && !$this->configMerger->isMergeRequired($envConfig)) {
+            return $this->configMerger->clear($envConfig);
+        }
+
+        $useSlave = $this->stageConfig->get(DeployInterface::VAR_MYSQL_USE_SLAVE_CONNECTION);
+
+        $config = [];
+        foreach (self::CONNECTION_MAP[self::CONNECTION] as $connectionName => $serviceKey) {
+            $connectionData = $this->getConnectionData($serviceKey);
+            if (empty($connectionData->getHost())) {
+                continue;
+            }
+            $config[self::CONNECTION][$connectionName] = $this->getConnectionConfig($connectionData);
+            if (!$useSlave && !isset(self::CONNECTION_MAP[self::SLAVE_CONNECTION][$connectionName])) {
+                continue;
+            }
+            $connectionData = $this->getConnectionData(self::CONNECTION_MAP[self::SLAVE_CONNECTION][$connectionName]);
+            if (empty($connectionData->getHost())
+                || !$this->isDbConfigCompatibleWithSlaveConnection($connectionName)) {
+                continue;
+            }
+            $config[self::SLAVE_CONNECTION][$connectionName] = $this->getConnectionConfig($connectionData, true);
+        }
+
+        return $this->configMerger->merge($config, $envConfig);
+    }
+
+    /**
+     * Returns resource configuration
+     * @param array $connections
+     * @return array
+     */
+    private function getResources(array $connections): array
+    {
+        $envConfig = $this->stageConfig->get(DeployInterface::VAR_RESOURCE_CONFIGURATION);
+
+        foreach (self::RESOURCE_MAP as $connectionName => $resourceName) {
+            if (in_array($connectionName, self::SPLIT_CONNECTIONS)
+                && isset($envConfig[$resourceName])) {
+                unset($envConfig[$resourceName]);
+            }
+        }
+
+        if (!$this->configMerger->isEmpty($envConfig) && !$this->configMerger->isMergeRequired($envConfig)) {
+            return $this->configMerger->clear($envConfig);
+        }
+
+        $config = [];
+
+        foreach ($connections as $connectionName) {
+            if (isset(self::RESOURCE_MAP[$connectionName])) {
+                $config[self::RESOURCE_MAP[$connectionName]][self::CONNECTION] = $connectionName;
+            }
+        }
+
+        return $this->configMerger->merge($config, $envConfig);
     }
 
     /**
@@ -156,16 +231,17 @@ class MergedConfig implements ConfigInterface
      * that doesn't match connection from relationships,
      * otherwise return false.
      *
+     * @param string $connectionName
      * @return boolean
      */
-    public function isDbConfigurationCompatibleWithSlaveConnection(): bool
+    public function isDbConfigCompatibleWithSlaveConnection(string $connectionName): bool
     {
-        $envDbConfig = $this->stageConfig->get(DeployInterface::VAR_DATABASE_CONFIGURATION);
-
-        if ((isset($envDbConfig['connection']['default']['host'])
-                && $envDbConfig['connection']['default']['host'] !== $this->getConnectionData()->getHost())
-            || (isset($envDbConfig['connection']['default']['dbname'])
-                && $envDbConfig['connection']['default']['dbname'] !== $this->getConnectionData()->getDbName())
+        $config = $this->stageConfig->get(DeployInterface::VAR_DATABASE_CONFIGURATION);
+        $connectionData = $this->getConnectionData(self::CONNECTION_MAP[self::CONNECTION][$connectionName]);
+        if ((isset($config[self::CONNECTION][$connectionName]['host'])
+                && $config[self::CONNECTION][$connectionName]['host'] !== $connectionData->getHost())
+            || (isset($config[self::CONNECTION][$connectionName]['dbname'])
+                && $config[self::CONNECTION][$connectionName]['dbname'] !== $connectionData->getDbName())
         ) {
             return false;
         }
@@ -174,28 +250,49 @@ class MergedConfig implements ConfigInterface
     }
 
     /**
-     * Returns db configuration from env.php.
+     * Returns connection data from relationship array
      *
-     * This method is calling only in case when database relationship configuration doesn't exist and
-     * database is not configured through .magento.env.yaml or env variable.
-     * It's workaround for scenarios when magento was installed by raw setup:install command not by deploy scripts.
+     * @param string $serviceKey
+     * @return ConnectionInterface
      */
-    private function getDbConfigFromEnvFile(): array
+    private function getConnectionData(string $serviceKey): ConnectionInterface
     {
-        return $this->configReader->read()['db'] ?? [];
+        if (!isset($this->connectionData[$serviceKey]) || !($this->connectionData[$serviceKey] instanceof ConnectionInterface)) {
+            $this->connectionData[$serviceKey] = $this->connectionDataFactory->create($serviceKey);
+        }
+
+        return $this->connectionData[$serviceKey];
     }
 
     /**
-     * Returns connection data from relationship array
+     * Returns configuration for connection
      *
-     * @return ConnectionInterface
+     * @param ConnectionInterface $connectionData
+     * @param bool $isSlave
+     * @return array
      */
-    private function getConnectionData(): ConnectionInterface
+    public function getConnectionConfig(ConnectionInterface $connectionData, $isSlave = false): array
     {
-        if (!$this->connectionData instanceof ConnectionInterface) {
-            $this->connectionData = $this->connectionFactory->create(RelationshipConnectionFactory::CONNECTION_MAIN);
+        $host = $connectionData->getHost();
+
+        if (!$host) {
+            return [];
         }
 
-        return $this->connectionData;
+        $port = $connectionData->getPort();
+
+        $config = [
+            'host' => empty($port) || $port == '3306' ? $host : $host . ':' . $port,
+            'username' => $connectionData->getUser(),
+            'dbname' => $connectionData->getDbName(),
+            'password' => $connectionData->getPassword(),
+        ];
+        if ($isSlave) {
+            $config['model'] = 'mysql4';
+            $config['engine'] = 'innodb';
+            $config['initStatements'] = 'SET NAMES utf8;';
+            $config['active'] = '1';
+        }
+        return $config;
     }
 }
