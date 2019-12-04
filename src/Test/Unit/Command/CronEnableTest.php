9e88440e01402c1b4e2c63834e6cc8843b847d4f
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-12-04 14:18:45 -0600

    MAGECLOUD-3756: Implement CLI commands to enable/disable cron manually (#642)

---
+++ src/Test/Unit/Command/CronEnableTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Unit\Command;
+
+use Magento\MagentoCloud\Command\CronEnable;
+use Magento\MagentoCloud\Cron\Switcher;
+use Magento\MagentoCloud\Filesystem\FileSystemException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @inheritDoc
+ */
+class CronEnableTest extends TestCase
+{
+    /**
+     * @var CronEnable
+     */
+    private $command;
+
+    /**
+     * @var LoggerInterface|MockObject
+     */
+    private $loggerMock;
+
+    /**
+     * @var Switcher|MockObject
+     */
+    private $cronSwitcherMock;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->loggerMock = $this->getMockForAbstractClass(LoggerInterface::class);
+        $this->cronSwitcherMock = $this->createMock(Switcher::class);
+
+        $this->command = new CronEnable($this->cronSwitcherMock, $this->loggerMock);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function testExecute()
+    {
+        $this->cronSwitcherMock->expects($this->once())
+            ->method('enable');
+        $this->loggerMock->expects($this->once())
+            ->method('info')
+            ->with('Enable cron');
+        $this->loggerMock->expects($this->never())
+            ->method('critical');
+
+        $tester = new CommandTester($this->command);
+        $tester->execute([]);
+        $this->assertSame(0, $tester->getStatusCode());
+    }
+
+    public function testExecuteWithException()
+    {
+        $this->expectException(FileSystemException::class);
+        $this->expectExceptionMessage('save error');
+
+        $this->cronSwitcherMock->expects($this->once())
+            ->method('enable')
+            ->willThrowException(new FileSystemException('save error'));
+        $this->loggerMock->expects($this->once())
+            ->method('critical')
+            ->with('save error');
+
+        $tester = new CommandTester($this->command);
+        $tester->execute([]);
+        $this->assertSame(1, $tester->getStatusCode());
+    }
+}
---
+++ src/Test/Unit/Cron/SwitcherTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Unit\Cron;
+
+use Magento\MagentoCloud\Config\Magento\Env\ReaderInterface;
+use Magento\MagentoCloud\Config\Magento\Env\WriterInterface;
+use Magento\MagentoCloud\Cron\Switcher;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @inheritDoc
+ */
+class SwitcherTest extends TestCase
+{
+    /**
+     * @var Switcher
+     */
+    private $switcher;
+
+    /**
+     * @var ReaderInterface|MockObject
+     */
+    private $readerMock;
+
+    /**
+     * @var WriterInterface|MockObject
+     */
+    private $writerMock;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $this->readerMock = $this->getMockForAbstractClass(ReaderInterface::class);
+        $this->writerMock = $this->getMockForAbstractClass(WriterInterface::class);
+
+        $this->switcher = new Switcher($this->writerMock, $this->readerMock);
+    }
+
+    /**
+     * @throws \Magento\MagentoCloud\Filesystem\FileSystemException
+     */
+    public function testDisable()
+    {
+        $this->writerMock->expects($this->once())
+            ->method('update')
+            ->with(['cron' => ['enabled' => 0]]);
+
+        $this->switcher->disable();
+    }
+
+    /**
+     * @throws \Magento\MagentoCloud\Filesystem\FileSystemException
+     */
+    public function testEnable()
+    {
+        $this->readerMock->expects($this->once())
+            ->method('read')
+            ->willReturn([
+                'option1' => 'value1',
+                'option2' => 'value2',
+                'cron' => ['enabled' => 0, 'some-option' => 'some-value']
+            ]);
+        $this->writerMock->expects($this->once())
+            ->method('create')
+            ->with([
+                'option1' => 'value1',
+                'option2' => 'value2',
+                'cron' => ['some-option' => 'some-value']
+            ]);
+
+        $this->switcher->enable();
+    }
+}
