5f05f4e68d2610509ccdc4eaf87c41b492278795
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2019-12-05 08:15:05 -0600

    MAGECLOUD-4269: Refactor ValidateConfiguration to pass string levels (#652)

--- src/Step/ValidateConfiguration.php
+++ src/Step/ValidateConfiguration.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\MagentoCloud\Step;
 
+use Magento\MagentoCloud\App\Logger;
 use Magento\MagentoCloud\Config\Validator\Result\Error;
 use Magento\MagentoCloud\Config\ValidatorInterface;
 use Psr\Log\LoggerInterface;
@@ -64,7 +65,8 @@ class ValidateConfiguration implements StepInterface
     }
 
     /**
-     * Returns all validation messages grouped by validation level
+     * Returns all validation messages grouped by validation level.
+     * Converts validation level to integer value using @see Logger::toMonologLevel() method
      *
      * @return array
      */
@@ -74,6 +76,7 @@ class ValidateConfiguration implements StepInterface
 
         /* @var $validators ValidatorInterface[] */
         foreach ($this->validators as $level => $validators) {
+            $level = Logger::toMonologLevel($level);
             foreach ($validators as $name => $validator) {
                 if (!$validator instanceof ValidatorInterface) {
                     $this->logger->info(sprintf('Validator "%s" was skipped', $name));
