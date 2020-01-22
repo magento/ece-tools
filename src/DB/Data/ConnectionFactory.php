0b2cca73857b9d4beef8994c1d51415842579395
Yevhen Miroshnychenko <miroshny@adobe.com>
2020-01-22 15:42:13 -0600

    MAGECLOUD-4890: Implement support for backup databases from split connections for `ece-tools`

--- src/DB/Data/ConnectionFactory.php
+++ src/DB/Data/ConnectionFactory.php
@@ -8,19 +8,22 @@ declare(strict_types=1);
 namespace Magento\MagentoCloud\DB\Data;
 
 use Magento\MagentoCloud\Config\Database\DbConfig;
+use RuntimeException;
 
 /**
  * Responsible for creating and configuring Magento\MagentoCloud\DB\Data\ConnectionInterface instances.
  */
-class ConnectionFactory
+class ConnectionFactory implements ConnectionFactoryInterface
 {
-    const CONNECTION_MAIN = 'main';
-    const CONNECTION_SLAVE = 'slave';
-
     /**
      * @var DbConfig
      */
     private $dbConfig;
+
+    /**
+     * @var array
+     */
+    private $config;
 
     /**
      * @param DbConfig $dbConfig
@@ -35,27 +38,47 @@ class ConnectionFactory
      *
      * @param string $connectionType
      * @return ConnectionInterface
-     * @throws \RuntimeException
+     * @throws RuntimeException
      */
     public function create(string $connectionType): ConnectionInterface
     {
         switch ($connectionType) {
             case self::CONNECTION_MAIN:
-                $connectionData = $this->dbConfig->get()['connection']['default'] ?? [];
-                $connection = new Connection($connectionData);
-                break;
+                return $this->getConnectionData(DbConfig::CONNECTION_DEFAULT);
             case self::CONNECTION_SLAVE:
-                $connectionData = $this->dbConfig->get()['slave_connection']['default']
-                    ?? $this->dbConfig->get()['connection']['default']
-                    ?? [];
-                $connection = new Connection($connectionData);
-                break;
+                return $this->getConnectionData(DbConfig::CONNECTION_DEFAULT, false);
+            case self::CONNECTION_QUOTE_MAIN:
+                return $this->getConnectionData(DbConfig::CONNECTION_CHECKOUT);
+            case self::CONNECTION_QUOTE_SLAVE:
+                return $this->getConnectionData(DbConfig::CONNECTION_CHECKOUT, false);
+            case self::CONNECTION_SALES_MAIN:
+                return $this->getConnectionData(DbConfig::CONNECTION_SALES);
+            case self::CONNECTION_SALES_SLAVE:
+                return $this->getConnectionData(DbConfig::CONNECTION_SALES, false);
             default:
-                throw new \RuntimeException(
+                throw new RuntimeException(
                     sprintf('Connection with type %s doesn\'t exist', $connectionType)
                 );
         }
+    }
 
-        return $connection;
+    private function getConnectionData($name, $isMain = true): ConnectionInterface
+    {
+        if ($isMain) {
+            $connectionConfig = $this->getConfig()[DbConfig::KEY_CONNECTION][$name] ?? [];
+        } else {
+            $connectionConfig = $this->getConfig()[DbConfig::KEY_SLAVE_CONNECTION][$name]
+                ?? $this->getConfig()[DbConfig::KEY_CONNECTION][$name]
+                ?? [];
+        }
+        return new Connection($connectionConfig);
+    }
+
+    private function getConfig(): array
+    {
+        if (null === $this->config) {
+            $this->config = $this->dbConfig->get();
+        }
+        return $this->config;
     }
 }
