6d635466bc69fbfbc02a7b2cbb812da799fd3059
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2020-01-22 14:42:32 -0600

    Update src/Command/DbDump.php
    Co-Authored-By: Margaret Eker <meker@adobe.com>

--- src/Command/DbDump.php
+++ src/Command/DbDump.php
@@ -115,7 +115,7 @@ class DbDump extends Command
             $helper = $this->getHelper('question');
 
             $questionParts = [
-                'The db-dump command switches the site to maintenance mode during the dump process.',
+                'The db-dump operation switches the site to maintenance mode, stops all active cron jobs and consumer queue processes and disables cron jobs before starting the the dump process.',
                 'Your site will not receive any traffic until the operation completes.',
                 'Do you wish to proceed with this process? (y/N)?',
             ];
