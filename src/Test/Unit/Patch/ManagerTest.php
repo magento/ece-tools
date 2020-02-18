42452fc8b8677a8547e3707cf45e70167711f9bf
Yevhen Miroshnychenko <miroshny@adobe.com>
2020-02-18 14:11:05 -0600

    MAGECLOUD-5310: Issue with Magento 2002.0 and latest patches release 1.0.1

--- src/Test/Unit/Patch/ManagerTest.php
+++ src/Test/Unit/Patch/ManagerTest.php
@@ -9,8 +9,10 @@ use Composer\Package\RootPackageInterface;
 use Magento\MagentoCloud\Config\GlobalSection;
 use Magento\MagentoCloud\Filesystem\DirectoryList;
 use Magento\MagentoCloud\Filesystem\Driver\File;
+use Magento\MagentoCloud\Filesystem\FileList;
 use Magento\MagentoCloud\Patch\Manager;
 use Magento\MagentoCloud\Shell\ProcessInterface;
+use Magento\MagentoCloud\Shell\ShellException;
 use Magento\MagentoCloud\Shell\ShellInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -57,6 +59,11 @@ class ManagerTest extends TestCase
     private $globalSectionMock;
 
     /**
+     * @var FileList|MockObject
+     */
+    private $fileListMock;
+
+    /**
      * @inheritdoc
      */
     protected function setUp()
@@ -67,82 +74,100 @@ class ManagerTest extends TestCase
         $this->fileMock = $this->createMock(File::class);
         $this->directoryListMock = $this->createMock(DirectoryList::class);
         $this->globalSectionMock = $this->createMock(GlobalSection::class);
+        $this->fileListMock = $this->createMock(FileList::class);
 
-        $this->directoryListMock->method('getMagentoRoot')
+        $this->directoryListMock->expects($this->once())
+            ->method('getMagentoRoot')
             ->willReturn('magento_root');
+        $this->fileListMock->expects($this->once())
+            ->method('getFrontStaticDist')
+            ->willReturn('dist/pub/front-static.php');
+        $this->fileMock->expects($this->once())
+            ->method('copy')
+            ->with(
+                'dist/pub/front-static.php',
+                'magento_root/pub/front-static.php'
+            );
+        $this->loggerMock->expects($this->once())
+            ->method('info')
+            ->with('File "front-static.php" was copied');
 
         $this->manager = new Manager(
             $this->loggerMock,
             $this->shellMock,
             $this->fileMock,
             $this->directoryListMock,
-            $this->globalSectionMock
+            $this->globalSectionMock,
+            $this->fileListMock
         );
     }
 
     public function testApply()
     {
-        $this->fileMock->expects($this->once())
-            ->method('isExists')
-            ->with('magento_root/pub/static.php')
-            ->willReturn(true);
-        $this->fileMock->expects($this->once())
-            ->method('copy')
-            ->with(
-                'magento_root/pub/static.php',
-                'magento_root/pub/front-static.php'
-            );
         $this->globalSectionMock->expects($this->once())
             ->method('get')
             ->with(GlobalSection::VAR_DEPLOYED_MAGENTO_VERSION_FROM_GIT)
             ->willReturn(false);
-
-        $processMock = $this->getMockForAbstractClass(ProcessInterface::class);
-        $processMock->method('getOutput')
-            ->willReturn('Some patch applied');
-
-        $this->shellMock->expects($this->once())
-            ->method('execute')
-            ->with('php ./vendor/bin/ece-patches apply')
-            ->willReturn($processMock);
-        $this->loggerMock->method('info')
-            ->withConsecutive(
-                ['File static.php was copied']
-            );
-        $this->loggerMock->method('notice')
+        $this->loggerMock->expects($this->exactly(2))
+            ->method('notice')
             ->withConsecutive(
                 ['Applying patches'],
                 ['End of applying patches']
             );
+        $processMock = $this->getMockForAbstractClass(ProcessInterface::class);
+        $processMock->method('getOutput')
+            ->willReturn('Some patch applied');
+        $this->shellMock->expects($this->once())
+            ->method('execute')
+            ->with('php ./vendor/bin/ece-patches apply')
+            ->willReturn($processMock);
 
         $this->manager->apply();
     }
 
     public function testApplyDeployedFromGitAndNoCopy()
     {
-        $this->fileMock->expects($this->once())
-            ->method('isExists')
-            ->with('magento_root/pub/static.php')
-            ->willReturn(false);
         $this->globalSectionMock->expects($this->once())
             ->method('get')
             ->with(GlobalSection::VAR_DEPLOYED_MAGENTO_VERSION_FROM_GIT)
             ->willReturn(true);
-
+        $this->loggerMock->method('notice')
+            ->withConsecutive(
+                ['Applying patches'],
+                ['End of applying patches']
+            );
         $processMock = $this->getMockForAbstractClass(ProcessInterface::class);
         $processMock->method('getOutput')
             ->willReturn('Some patch applied');
-
         $this->shellMock->expects($this->once())
             ->method('execute')
             ->with('php ./vendor/bin/ece-patches apply --git-installation 1')
             ->willReturn($processMock);
-        $this->loggerMock->method('notice')
-            ->withConsecutive(
-                ['File static.php was not found'],
-                ['Applying patches'],
-                ['End of applying patches']
-            );
+
+        $this->manager->apply();
+    }
+
+    public function testApplyWithException()
+    {
+        $this->globalSectionMock->expects($this->once())
+            ->method('get')
+            ->with(GlobalSection::VAR_DEPLOYED_MAGENTO_VERSION_FROM_GIT)
+            ->willReturn(false);
+        $this->loggerMock->expects($this->once())
+            ->method('notice')
+            ->with('Applying patches');
+        $processMock = $this->getMockForAbstractClass(ProcessInterface::class);
+        $processMock->method('getOutput')
+            ->willReturn('Some patch applied');
+        $exception = new ShellException('Some Error');
+        $this->shellMock->expects($this->once())
+            ->method('execute')
+            ->with('php ./vendor/bin/ece-patches apply')
+            ->willThrowException($exception);
+        $this->loggerMock->expects($this->once())
+            ->method('error')
+            ->with('Some Error');
+        $this->expectExceptionObject($exception);
 
         $this->manager->apply();
     }
