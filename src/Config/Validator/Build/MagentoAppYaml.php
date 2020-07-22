2c17f5ba2667c1b71b09cf7a7cc2490bd452670e
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2020-07-22 09:27:09 -0500

    MCLOUD-6485: Add validator for braintree variable existence (#759)

---
+++ src/Config/Validator/Build/MagentoAppYaml.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Config\Validator\Build;
+
+use Magento\MagentoCloud\App\Error as AppError;
+use Magento\MagentoCloud\Config\Environment;
+use Magento\MagentoCloud\Config\Validator;
+use Magento\MagentoCloud\Config\ValidatorException;
+use Magento\MagentoCloud\Config\ValidatorInterface;
+use Magento\MagentoCloud\Package\MagentoVersion;
+
+/**
+ * Validates that .magento.app.yaml contains correct configuration:
+ * - CONFIG__STORES__DEFAULT__PAYMENT__BRAINTREE__CHANNEL must be absent in .magento.app.yaml env variables
+ *   for Magento >= 2.4.0
+ *
+ */
+class MagentoAppYaml implements ValidatorInterface
+{
+    /**
+     * @var Environment
+     */
+    private $environment;
+
+    /**
+     * @var MagentoVersion
+     */
+    private $magentoVersion;
+
+    /**
+     * @var Validator\ResultFactory
+     */
+    private $resultFactory;
+
+    /**
+     *
+     * @param Environment $environment
+     * @param MagentoVersion $magentoVersion
+     * @param Validator\ResultFactory $resultFactory
+     */
+    public function __construct(
+        Environment $environment,
+        MagentoVersion $magentoVersion,
+        Validator\ResultFactory $resultFactory
+    ) {
+        $this->environment = $environment;
+        $this->magentoVersion = $magentoVersion;
+        $this->resultFactory = $resultFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate(): Validator\ResultInterface
+    {
+        try {
+            if ($this->magentoVersion->satisfies('>= 2.4.0')) {
+                $appData = $this->environment->getApplication();
+                if (isset($appData['variables']['env']['CONFIG__STORES__DEFAULT__PAYMENT__BRAINTREE__CHANNEL'])) {
+                    return $this->resultFactory->errorByCode(AppError::BUILD_WRONG_BRAINTREE_VARIABLE);
+                }
+            }
+
+            return $this->resultFactory->success();
+        } catch (\Exception $e) {
+            throw new ValidatorException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
--- src/Config/Validator/ResultFactory.php
+++ src/Config/Validator/ResultFactory.php
@@ -7,14 +7,29 @@ declare(strict_types=1);
 
 namespace Magento\MagentoCloud\Config\Validator;
 
+use Magento\MagentoCloud\App\ErrorInfo;
 use Magento\MagentoCloud\Config\Validator\Result\Error;
 use Magento\MagentoCloud\Config\Validator\Result\Success;
+use Magento\MagentoCloud\Filesystem\FileSystemException;
 
 /**
  * Creates instance of ResultInterface object
  */
 class ResultFactory
 {
+    /**
+     * @var ErrorInfo
+     */
+    private $errorInfo;
+
+    /**
+     * @param ErrorInfo $errorInfo
+     */
+    public function __construct(ErrorInfo $errorInfo)
+    {
+        $this->errorInfo = $errorInfo;
+    }
+
     /**
      * Creates instance of ResultInterface object
      *
@@ -52,5 +67,24 @@ class ResultFactory
     public function error(string $message, string $suggestion = '', int $code = null): Error
     {
         return new Error($message, $suggestion, $code);
+    }
+
+    /**
+     * Creates Error object, fetches info about error from .schema.error.yaml file by code.
+     * Uses for errors with static error message and suggestion.
+     *
+     * @param int $code
+     * @return Error
+     * @throws FileSystemException
+     */
+    public function errorByCode(int $code): Error
+    {
+        $errorData = $this->errorInfo->get($code);
+
+        return new Error(
+            $errorData['title'] ?? '',
+            $errorData['suggestion'] ?? '',
+            $code
+        );
     }
 }
