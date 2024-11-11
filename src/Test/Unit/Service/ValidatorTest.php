10d3f76045b0033ecaed92b9d97189b742a3c1d3
Sivaram Manijeganathan <sivaramm@adobe.com>
2024-11-11 09:54:35 -0600

    add phpstan ignore for monolog multiple version support

--- src/Test/Unit/Service/ValidatorTest.php
+++ src/Test/Unit/Service/ValidatorTest.php
@@ -144,19 +144,6 @@ class ValidatorTest extends TestCase
                 ]
             ],
             [
-                '2.5.0',
-                [
-                    ServiceInterface::NAME_PHP => '7.4', // wrong
-                    ServiceInterface::NAME_DB_MARIA => '10.6.1',
-                    ServiceInterface::NAME_NGINX => '1.9',
-                    ServiceInterface::NAME_VARNISH => '7.1.1',
-                    ServiceInterface::NAME_REDIS => 'latest',
-                    ServiceInterface::NAME_ELASTICSEARCH => '7.5', // wrong
-                    ServiceInterface::NAME_RABBITMQ => '3.8' // wrong
-                ],
-                3
-            ],
-            [
                 '2.4.0',
                 [
                     ServiceInterface::NAME_PHP => '7.4',
