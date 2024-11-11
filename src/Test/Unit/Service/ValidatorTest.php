ac76f361d6402492d8dfc576c1022d6e8229baf5
Sivaram Manijeganathan <sivaramm@adobe.com>
2024-11-11 09:16:56 -0600

    fix unit test

--- src/Test/Unit/Service/ValidatorTest.php
+++ src/Test/Unit/Service/ValidatorTest.php
@@ -146,13 +146,13 @@ class ValidatorTest extends TestCase
             [
                 '2.5.0',
                 [
-                    ServiceInterface::NAME_PHP => '7.2.13', // wrong
+                    ServiceInterface::NAME_PHP => '7.4', // wrong
                     ServiceInterface::NAME_DB_MARIA => '10.6.1',
                     ServiceInterface::NAME_NGINX => '1.9',
                     ServiceInterface::NAME_VARNISH => '7.1.1',
                     ServiceInterface::NAME_REDIS => 'latest',
-                    ServiceInterface::NAME_ELASTICSEARCH => '6.7', // wrong
-                    ServiceInterface::NAME_RABBITMQ => '3.7' // wrong
+                    ServiceInterface::NAME_ELASTICSEARCH => '7.5', // wrong
+                    ServiceInterface::NAME_RABBITMQ => '3.8' // wrong
                 ],
                 3
             ],
