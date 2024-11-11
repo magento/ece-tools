417b30361b7a3b95095d60a8a40f11bf91f1a32e
Sivaram Manijeganathan <sivaramm@adobe.com>
2024-11-11 12:22:27 -0600

    Remove duplicate code from 83 tests

--- src/Test/Functional/Acceptance/ErrorMessage83Cest.php
+++ src/Test/Functional/Acceptance/ErrorMessage83Cest.php
@@ -20,17 +20,4 @@ class ErrorMessage83Cest extends AbstractCest
      * @var string
      */
     protected $magentoCloudTemplate = '2.4.7';
-
-    /**
-     * @param CliTester $I
-     * @throws \Robo\Exception\TaskException
-     */
-    public function testShellErrorMessage(CliTester $I): void
-    {
-        $I->generateDockerCompose('--mode=production');
-        $I->runDockerComposeCommand('run build cloud-build');
-        $I->cleanDirectories(['/bin/*']);
-        $I->assertFalse($I->runDockerComposeCommand('run build ece-command build'));
-        $I->seeInOutput('Could not open input file: ./bin/magento');
-    }
 }
--- src/Test/Functional/Acceptance/ScdMatrix83Cest.php
+++ src/Test/Functional/Acceptance/ScdMatrix83Cest.php
@@ -18,36 +18,4 @@ class ScdMatrix83Cest extends ScdMatrixCest
      * @var string
      */
     protected $magentoCloudTemplate = '2.4.7';
-
-    /**
-     * @param \CliTester $I
-     * @param \Codeception\Example $data
-     * @throws \Robo\Exception\TaskException
-     * @dataProvider scdOnDeployDataProvider
-     */
-    public function testScdOnDeploy(\CliTester $I, \Codeception\Example $data): void
-    {
-        $I->copyFileToWorkDir($data['env_yaml'], 'magento.env.yaml');
-        $I->generateDockerCompose('--mode=production');
-        $I->runDockerComposeCommand('run build cloud-build');
-        $I->startEnvironment();
-        $I->runDockerComposeCommand('run deploy cloud-deploy');
-        $I->runDockerComposeCommand('run deploy cloud-post-deploy');
-
-        $I->amOnPage('/');
-        $I->see('Home page');
-        $I->see('CMS homepage content goes here.');
-    }
-
-    /**
-     * @return array
-     */
-    protected function scdOnDeployDataProvider(): array
-    {
-        return [
-            ['env_yaml' => 'files/scd/env_matrix_1.yaml'],
-            ['env_yaml' => 'files/scd/env_matrix_2.yaml'],
-            ['env_yaml' => 'files/scd/env_matrix_3.yaml'],
-        ];
-    }
 }
