229ada3e74768bdce7930e360815d04babd09408
ashvini22326 <pru22326@adobe.com>
2025-02-24 20:57:39 +0530

    added php8.4 support cases

---
+++ src/Test/Functional/Acceptance/ErrorMessage84Cest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Functional\Acceptance;
+
+use CliTester;
+
+/**
+ * This test runs on the latest version of PHP
+ *
+ * @group php84
+ */
+class ErrorMessage84Cest extends ErrorMessageCest
+{
+    /**
+     * @var string
+     */
+    protected $magentoCloudTemplate = '2.4.8';
+}
