38362f97bd3f45550643debcb3ceb204c732c7e9
ashvini22326 <pru22326@adobe.com>
2025-03-18 18:43:26 +0530

    Valkey Unit Test

--- src/Test/Unit/Service/EolValidatorTest.php
+++ src/Test/Unit/Service/EolValidatorTest.php
@@ -172,6 +172,14 @@ class EolValidatorTest extends TestCase
         $service6->expects($this->once())
             ->method('getVersion')
             ->willReturn('10.2');
+      $service7 = $this->createMock(ServiceInterface::class);
+      $service7->expects($this->once())
+        ->method('getVersion')
+        ->willReturn('8.0');
+      $service8 = $this->createMock(ServiceInterface::class);
+      $service8->expects($this->once())
+        ->method('getVersion')
+        ->willReturn('8.0');
         // withConsecutive() alternative.
         $this->serviceFactoryMock->expects($this->exactly(6))
             ->method('create')
@@ -181,7 +189,9 @@ class EolValidatorTest extends TestCase
                 'rabbitmq' => $service3,
                 'redis' => $service4,
                 'redis-session' => $service5,
-                'mariadb' => $service6
+                'mariadb' => $service6,
+                'valkey' => $service7,
+                'valkey-session' => $service8,
             });
 
         $this->assertEquals(
---
+++ src/Test/Unit/Service/ValkeySessionTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Unit\Service;
+
+use Magento\MagentoCloud\Config\Environment;
+use Magento\MagentoCloud\Service\Valkey\Version;
+use Magento\MagentoCloud\Service\ValkeySession;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @inheritDoc
+ */
+class ValkeySessionTest extends TestCase
+{
+  /**
+   * @var ValkeySession
+   */
+  private $valkeySession;
+
+  /**
+   * @var Environment|MockObject
+   */
+  private $environmentMock;
+
+  /**
+   * @var Version|MockObject
+   */
+  private $versionRetrieverMock;
+
+  /**
+   * @inheritDoc
+   */
+  public function setUp(): void
+  {
+    $this->environmentMock = $this->createMock(Environment::class);
+    $this->versionRetrieverMock = $this->createMock(Version::class);
+
+    $this->valkeySession = new ValkeySession(
+      $this->environmentMock,
+      $this->versionRetrieverMock
+    );
+  }
+
+  public function testGetConfiguration(): void
+  {
+    $this->environmentMock->expects($this->once())
+      ->method('getRelationship')
+      ->with(ValkeySession::RELATIONSHIP_SESSION_KEY)
+      ->willReturn([
+        [
+          'host' => '127.0.0.1',
+          'port' => '3306',
+        ]
+      ]);
+
+    $this->assertSame(
+      [
+        'host' => '127.0.0.1',
+        'port' => '3306',
+      ],
+      $this->valkeySession->getConfiguration()
+    );
+  }
+
+  public function testGetVersion(): void
+  {
+    $version = '1.1.1';
+    $config = [['some config']];
+
+    $this->environmentMock->expects($this->once())
+      ->method('getRelationship')
+      ->willReturn($config);
+
+    $this->versionRetrieverMock->expects($this->once())
+      ->method('getVersion')
+      ->with($config[0])
+      ->willReturn($version);
+    $this->assertSame(
+      $version,
+      $this->valkeySession->getVersion()
+    );
+  }
+}
