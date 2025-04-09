a6334aa2d8bb43510a8a0e77dd5162468715b366
ashvini22326 <pru22326@adobe.com>
2025-04-09 18:06:41 +0530

    valkey debug for 2.4.7

--- src/Step/Deploy/PreDeploy/ConfigUpdate/Cache.php
+++ src/Step/Deploy/PreDeploy/ConfigUpdate/Cache.php
@@ -187,7 +187,8 @@ class Cache implements StepInterface
         $notAllowedValkeyBackend = [
             CacheFactory::VALKEY_BACKEND_VALKEY_CACHE
         ];
-
+     $isValkeyEnabled=  $this->cacheConfig->isValkeyEnabled();
+     print_r($isValkeyEnabled);
         try {
             if (in_array($backend, $notAllowedValkeyBackend, true)
               && !$this->magentoVersion->isGreaterOrEqual('2.4.8')) {
