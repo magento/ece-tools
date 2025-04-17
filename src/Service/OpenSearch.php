1810b86946aabb742d309357a6b3d91472bb49bf
Prateek Karanpuria <pru88413@adobe.com>
2025-04-17 18:01:47 +0530

    MCLOUD-13607: Fix for ece-tools (>=v2002.2.2) as it breaks search engine configuration on 2.4.4 and 2.4.5

--- src/Service/OpenSearch.php
+++ src/Service/OpenSearch.php
@@ -51,7 +51,9 @@ class OpenSearch extends AbstractService implements ServiceInterface
      */
     public function getFullEngineName(): string
     {
-        if ($this->magentoVersion->isGreaterOrEqual('2.4.4')) {
+        if ($this->magentoVersion->isGreaterOrEqual('2.4.6')
+           || $this->magentoVersion->getVersion() == '2.4.4-p13'
+           || $this->magentoVersion->getVersion() == '2.4.5-p12') {
             return static::ENGINE_NAME;
         }
 
