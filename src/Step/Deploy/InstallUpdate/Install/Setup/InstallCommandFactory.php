11da6085359e739f3958b6bd2ad016a5d6bae19c
Prateek Karanpuria <pru88413@adobe.com>
2025-04-17 19:32:31 +0530

    MCLOUD-13607: Additional fix for ece-tools (>=v2002.2.2) as it breaks search engine configuration on 2.4.4 and 2.4.5

--- src/Step/Deploy/InstallUpdate/Install/Setup/InstallCommandFactory.php
+++ src/Step/Deploy/InstallUpdate/Install/Setup/InstallCommandFactory.php
@@ -268,7 +268,9 @@ class InstallCommandFactory
                 $configuration = $this->openSearch->getConfiguration();
                 $isAuthEnabled = $this->openSearch->isAuthEnabled();
 
-                if ($this->magentoVersion->isGreaterOrEqual('2.4.6')) {
+                ($this->magentoVersion->isGreaterOrEqual('2.4.6')
+                || $this->magentoVersion->getVersion() == '2.4.4-p13'
+                || $this->magentoVersion->getVersion() == '2.4.5-p12') {
                     $enginePrefixName = 'opensearch';
                 }
             } else {
