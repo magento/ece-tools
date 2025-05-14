8fcfd705e9986230b9379c2d2fe0468cfd87338c
Prateek Karanpuria <pru88413@adobe.com>
2025-05-14 17:32:31 +0530

    MCLOUD-13455: ServiceVersion test class unit test fixes

--- src/Test/Unit/Config/Validator/Deploy/ServiceVersionTest.php
+++ src/Test/Unit/Config/Validator/Deploy/ServiceVersionTest.php
@@ -10,9 +10,9 @@ namespace Magento\MagentoCloud\Test\Unit\Config\Validator\Deploy;
 use Magento\MagentoCloud\Config\Validator\Deploy\ServiceVersion;
 use Magento\MagentoCloud\Config\ValidatorException;
 use Magento\MagentoCloud\Service\Detector\DatabaseType;
-use Magento\MagentoCloud\Service\ServiceMismatchException;
-use Magento\MagentoCloud\Service\ServiceInterface;
 use Magento\MagentoCloud\Service\ServiceFactory;
+use Magento\MagentoCloud\Service\ServiceInterface;
+use Magento\MagentoCloud\Service\ServiceMismatchException;
 use Magento\MagentoCloud\Service\Validator as ServiceVersionValidator;
 use Magento\MagentoCloud\Config\Validator\Result\Error;
 use Magento\MagentoCloud\Config\Validator\Result\Success;
@@ -190,7 +190,9 @@ class ServiceVersionTest extends TestCase
             'error message 3',
             'error message 4',
             'error message 5',
-            'error message 6'
+            'error message 6',
+            'error message 7',
+            'error message 8',
         ];
         $service1 = $this->createMock(ServiceInterface::class);
         $service1->expects($this->once())
@@ -206,20 +208,37 @@ class ServiceVersionTest extends TestCase
             ->willReturn('2.2');
         $service4 = $this->createMock(ServiceInterface::class);
         $service4->expects($this->once())
-            ->method('getVersion')
-            ->willReturn('7.7');
+                ->method('getVersion')
+                ->willReturn('8.0');
         $service5 = $this->createMock(ServiceInterface::class);
         $service5->expects($this->once())
-            ->method('getVersion')
-            ->willReturn('1.2');
+                ->method('getVersion')
+                ->willReturn('8.0');
         $service6 = $this->createMock(ServiceInterface::class);
         $service6->expects($this->once())
             ->method('getVersion')
+            ->willReturn('7.7');
+        $service7 = $this->createMock(ServiceInterface::class);
+        $service7->expects($this->once())
+            ->method('getVersion')
+            ->willReturn('1.2');
+        $service8 = $this->createMock(ServiceInterface::class);
+        $service8->expects($this->once())
+            ->method('getVersion')
             ->willReturn('5.7');
-        $this->serviceFactory->expects($this->exactly(6))
+        $this->serviceFactory->expects($this->exactly(8))
             ->method('create')
-            ->willReturnOnConsecutiveCalls($service1, $service2, $service3, $service4, $service5, $service6);
-        $this->serviceVersionValidatorMock->expects($this->exactly(6))
+            ->willReturnOnConsecutiveCalls(
+                $service1,
+                $service2,
+                $service3,
+                $service4,
+                $service5,
+                $service6,
+                $service7,
+                $service8,
+        );
+        $this->serviceVersionValidatorMock->expects($this->exactly(8))
             ->method('validateService')
             // withConsecutive() alternative.
             ->willReturnCallback(
@@ -231,16 +250,18 @@ class ServiceVersionTest extends TestCase
                     } elseif ($arg1 == ServiceInterface::NAME_REDIS_SESSION && $arg2 == '2.2') {
                         return $errorMessages[2];
                     } elseif ($arg1 == ServiceInterface::NAME_VALKEY && $arg2 == '8.0') {
-                        return $errorMessages[1];
-                    } elseif ($arg1 == ServiceInterface::NAME_VALKEY_SESSION && $arg2 == '8.0') {
-                        return $errorMessages[2];
-                    } elseif ($arg1 == ServiceInterface::NAME_ELASTICSEARCH && $arg2 == '7.7') {
                         return $errorMessages[3];
-                    } elseif ($arg1 == ServiceInterface::NAME_OPENSEARCH && $arg2 == '1.2') {
+                    } elseif ($arg1 == ServiceInterface::NAME_VALKEY_SESSION && $arg2 == '8.0') {
                         return $errorMessages[4];
-                    } elseif ($arg1 == ServiceInterface::NAME_DB_MYSQL && $arg2 == '5.7') {
+                    } elseif ($arg1 == ServiceInterface::NAME_ELASTICSEARCH && $arg2 == '7.7') {
                         return $errorMessages[5];
+                    } elseif ($arg1 == ServiceInterface::NAME_OPENSEARCH && $arg2 == '1.2') {
+                        return $errorMessages[6];
+                    } elseif ($arg1 == ServiceInterface::NAME_DB_MYSQL && $arg2 == '5.7') {
+                        return $errorMessages[7];
                     }
+
+                    return '';
                 }
             );
         $this->resultFactoryMock->expects($this->once())
