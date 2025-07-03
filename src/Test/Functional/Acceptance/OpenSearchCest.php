01ea1300fa4a1300c4334b9eed0702f24ef42d04
shruthi malode <del21738@adobe.com>
2025-07-03 12:21:38 +0530

    MCLOUD-13769: Create functional test for opensearch in ece-tools

---
+++ src/Test/Functional/Acceptance/OpenSearchCest.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Functional\Acceptance;
+
+use Magento\CloudDocker\Test\Functional\Codeception\Docker;
+
+/**
+ * This test runs on the latest version of PHP
+ *
+ * @group php74
+ */
+class OpenSearchCest extends AbstractCest
+{
+    /**
+     * @param \CliTester $I
+     */
+    public function _before(\CliTester $I): void
+    {
+        // Do nothing
+    }
+
+    /**
+     * @param \CliTester $I
+     * @param \Codeception\Example $data
+     * @throws \Robo\Exception\TaskException
+     * @dataProvider openDataProvider
+     * @skip Skip Need to fix OpenSearch containers
+     */
+    public function testOpen(\CliTester $I, \Codeception\Example $data): void
+    {
+        $this->removeEs = $data['removeES'];
+
+        $this->prepareWorkplace($I, $data['magento']);
+
+        $I->generateDockerCompose('--mode=production');
+
+        $I->runDockerComposeCommand('run build cloud-build');
+        $I->startEnvironment();
+        $I->runDockerComposeCommand('run deploy cloud-deploy');
+
+        $I->runDockerComposeCommand(
+            'run deploy magento-command config:set general/region/state_required US --lock-env'
+        );
+        $this->checkConfigurationIsNotRemoved($I);
+
+        $I->amOnPage('/');
+        $I->see('Home page');
+
+        $config = $this->getConfig($I);
+        $this->checkArraySubset(
+            $data['expectedResult'],
+            $config['system']['default']['catalog']['search'],
+            $I
+        );
+
+        $I->assertTrue($I->cleanDirectories(['/vendor/*', '/setup/*']));
+        $I->stopEnvironment(true);
+        $this->removeEs = true;
+        $this->removeESIfExists($I);
+
+        $I->generateDockerCompose('--mode=production');
+
+        $I->runDockerComposeCommand('run build cloud-build');
+        $I->startEnvironment();
+        $I->runDockerComposeCommand('run deploy cloud-deploy');
+
+        $this->checkConfigurationIsNotRemoved($I);
+
+        $I->amOnPage('/');
+        $I->see('Home page');
+
+        $config = $this->getConfig($I);
+        $this->checkArraySubset(
+            ['engine' => 'mysql'],
+            $config['system']['default']['catalog']['search'],
+            $I
+        );
+    }
+
+    /**
+     * @param \CliTester $I
+     * @return array
+     */
+    private function getConfig(\CliTester $I): array
+    {
+        $destination = sys_get_temp_dir() . '/app/etc/env.php';
+        $I->assertTrue($I->downloadFromContainer('/app/etc/env.php', $destination, Docker::DEPLOY_CONTAINER));
+        return require $destination;
+    }
+
+    /**
+     * @param \CliTester $I
+     * @return array
+     */
+    private function checkConfigurationIsNotRemoved(\CliTester $I): void
+    {
+        $config = $this->getConfig($I);
+        $this->checkArraySubset(
+            ['general' => ['region' => ['state_required' => 'US']]],
+            $config['system']['default'],
+            $I
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function openDataProvider(): array
+    {
+        return [
+            [
+                'magento' => '2.4.3',
+                'removeES' => false,
+                'expectedResult' => [
+                    'engine' => 'opensearch6',
+                    'opensearch6_server_hostname' => 'opensearch',
+                    'opensearch6_server_port' => '9200'
+                ],
+            ],
+        ];
+    }
+}
