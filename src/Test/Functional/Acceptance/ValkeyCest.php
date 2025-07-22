cd3f471bb880a618607b2b3cbc0c4bca1af0e00e
ashvini22326 <pru22326@adobe.com>
2025-07-22 15:48:36 +0530

    updated test

--- src/Test/Functional/Acceptance/ValkeyCest.php
+++ src/Test/Functional/Acceptance/ValkeyCest.php
@@ -56,7 +56,7 @@ abstract class ValkeyCest extends AbstractCest
         $config = $this->getConfig($I);
 
         $I->assertSame(
-            '\Magento\Framework\Cache\Backend\Redis',
+            'Cm_Cache_Backend_Redis',
             $config['cache']['frontend']['default']['backend'],
             'Wrong backend model'
         );
