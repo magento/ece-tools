838efda4d5d539c14b050c75d0f3ad5e731f6a5e
andrii_shevtsov <ashevtso@adobe.com>
2025-08-02 15:16:39 -0500

    Added  explicit string type declaration  for 8.1/8.2

--- src/Test/Functional/Acceptance/ScenarioExtensibility82Cest.php
+++ src/Test/Functional/Acceptance/ScenarioExtensibility82Cest.php
@@ -19,5 +19,5 @@ class ScenarioExtensibility82Cest extends ScenarioExtensibilityCest
     /**
      * @var string
      */
-    protected $magentoCloudTemplate = '2.4.6';
+    protected string $magentoCloudTemplate = '2.4.6';
 }
