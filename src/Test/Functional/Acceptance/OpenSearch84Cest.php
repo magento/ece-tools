0197ed07df9ec1275caba277c8485bffcf3c1b3d
Prateek Karanpuria <pru88413@adobe.com>
2025-08-13 18:34:59 +0530

    MCLOUD-13611: Extend opensearch test for php 8.1, 8.2, 8.3 & 8.4 test group

---
+++ src/Test/Functional/Acceptance/OpenSearch84Cest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Functional\Acceptance;
+
+/**
+ * This test runs on the latest version of PHP
+ *
+ * @group php84
+ */
+class OpenSearch84Cest extends OpenSearchCest
+{
+    /**
+     * @var string
+     */
+    protected string $magentoCloudTemplate = '2.4.8';
+}
