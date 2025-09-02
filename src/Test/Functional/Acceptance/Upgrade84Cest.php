95f91a87f7d48344d0ef0e0a5f387928e9125a84
vishalakshi <glo33270@adobe.com>
2025-09-02 18:38:25 +0530

    MCLOUD-14027:Adjust versions for UpgradeCest Test in Ece-tool

---
+++ src/Test/Functional/Acceptance/Upgrade84Cest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Magento\MagentoCloud\Test\Functional\Acceptance;
+
+/**
+ * Upgrade test for PHP 8.4
+ *
+ * @group php84
+ */
+class Upgrade84Cest extends UpgradeCest
+{
+    /**
+     * Provides upgrade test cases for PHP 8.4-supported Magento versions.
+     *
+     * @return array
+     */
+    protected function testProvider(): array
+    {
+        return [
+            ['from' => '2.4.8', 'to' => '>=2.4.8-p1 <2.4.9'],
+            ['from' => '2.4.8-p1', 'to' => '>=2.4.9'],
+        ];
+    }
+}
