4ed8e1916985bbf1dcd52556235952df7bfb3bea
shruthi malode <del21738@adobe.com>
2025-09-25 14:12:05 +0530

    MCLOUD-14015:Changes in AdminCredentialCest

--- src/Test/Functional/Acceptance/DataTypesOptionValidationCest.php
+++ src/Test/Functional/Acceptance/DataTypesOptionValidationCest.php
@@ -34,10 +34,8 @@ abstract class DataTypesOptionValidationCest extends AbstractCest
         );
         $I->runDockerComposeCommand('run build cloud-build');
         $I->startEnvironment();
-        // force bad values
         $I->runDockerComposeCommand('run deploy cloud-deploy');
         $log = $I->grabFileContent('/var/log/cloud.error.log');
-        error_log("log: " . $log);
         $I->assertStringContainsString($data['expectedError'], $log);
     }
 
